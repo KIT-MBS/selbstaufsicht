e2a617319ab38278d9b9bcb2b83a1ccdf9ea9b9e
Oskar Taubert <oskar.taubert@kit.edu>
2021-03-17 17:07:22 +0100

    cosmetics

--- selbstaufsicht/models/__init__.py
+++ selbstaufsicht/models/__init__.py
@@ -7,14 +7,14 @@ class TransformerEncoderStack(nn.Module):
         super(TransformerEncoderStack, self).__init__()
         self.positional_encoding = SinoidalPositionalEncodingSequence(d, dropout)
         self.dinput = dinput
-        encoder_layer = TransformerEncoderLayer(d, nhead, dff, dropout)
-        self.encoder = TransformerEncoder(encoder_layer, nlayers)
+        encoder_layer = nn.TransformerEncoderLayer(d, nhead, dff, dropout)
+        self.encoder = nn.TransformerEncoder(encoder_layer, nlayers)
         self.embedding = nn.Embedding(dinput, d)
         self.output_head = nn.Linear(d, dinput)
 
-    def forward(self, src):
-        src = self.embedding(src) * math.sqrt(self.dinput)
-        src = self.positional_encoding(src)
-        src = self.encoder(src)
-        src = self.head(src)
-        return src
+    def forward(self, x):
+        x = self.embedding(x) * math.sqrt(self.dinput)
+        x = self.positional_encoding(x)
+        x = self.encoder(x)
+        x = self.head(x)
+        return x
