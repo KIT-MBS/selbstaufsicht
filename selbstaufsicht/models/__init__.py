c1af0e1d054aa23666925a39d414f685a1119719
Oskar Taubert <oskar.taubert@kit.edu>
2021-03-26 14:36:03 +0100

    added rna to tensor endoding

--- selbstaufsicht/models/__init__.py
+++ selbstaufsicht/models/__init__.py
@@ -18,3 +18,35 @@ class TransformerEncoderStack(nn.Module):
         x = self.encoder(x)
         x = self.head(x)
         return x
+
+# ambuguous RNA letters: GAUCRYWSMKHBVDN
+rna_to_tensor_dict = {
+        '-': torch.tensor([0., 0., 0., 0.]),
+        '.': torch.tensor([0., 0., 0., 0.]),
+        'G': torch.tensor([1., 0., 0., 0.]),
+        'A': torch.tensor([0., 1., 0., 0.]),
+        'U': torch.tensor([0., 0., 1., 0.]),
+        'C': torch.tensor([0., 0., 0., 1.]),
+        'R': torch.tensor([1., 1., 0., 0.])/2.,
+        'Y': torch.tensor([0., 0., 1., 1.])/2.,
+        'W': torch.tensor([0., 1., 1., 0.])/2.,
+        'S': torch.tensor([1., 0., 0., 1.])/2.,
+        'M': torch.tensor([0., 1., 0., 1.])/2.,
+        'K': torch.tensor([1., 0., 1., 0.])/2.,
+        'H': torch.tensor([0., 1., 1., 1.])/3.,
+        'B': torch.tensor([1., 0., 1., 1.])/3.,
+        'V': torch.tensor([1., 1., 0., 1.])/3.,
+        'D': torch.tensor([1., 1., 1., 0.])/3.,
+        'N': torch.tensor([1., 1., 1., 1.])/4.,
+        }
+
+
+class EncoderStackTransforms():
+    def __init__(self):
+        pass
+
+    def __call__(self, msa):
+        S = len(msa)
+        L = msa.get_alignment_length()
+        A = 4
+        x = torch.empy()
