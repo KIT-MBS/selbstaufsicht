cd5129efddc2d1459ddf9b0f257b5d18322ac999
Oskar Taubert <oskar.taubert@kit.edu>
2021-05-05 00:34:17 +0200

    collate and sort of tokenization

--- selbstaufsicht/utils.py
+++ selbstaufsicht/utils.py
@@ -1,41 +1,48 @@
 import torch
 
-
 # ambuguous RNA letters: GAUCRYWSMKHBVDN
+rna_letters = [letter for letter in '-GAUCRYWSMKHBVDN']
+rna_to_index = {letter: index for index, letter in enumerate(rna_letters)}
+
 rna_to_tensor_dict = {
-        '-': torch.tensor([0., 0., 0., 0.]),
-        '.': torch.tensor([0., 0., 0., 0.]),
-        'G': torch.tensor([1., 0., 0., 0.]),
-        'A': torch.tensor([0., 1., 0., 0.]),
-        'U': torch.tensor([0., 0., 1., 0.]),
-        'C': torch.tensor([0., 0., 0., 1.]),
-        'R': torch.tensor([1., 1., 0., 0.])/2.,
-        'Y': torch.tensor([0., 0., 1., 1.])/2.,
-        'W': torch.tensor([0., 1., 1., 0.])/2.,
-        'S': torch.tensor([1., 0., 0., 1.])/2.,
-        'M': torch.tensor([0., 1., 0., 1.])/2.,
-        'K': torch.tensor([1., 0., 1., 0.])/2.,
-        'H': torch.tensor([0., 1., 1., 1.])/3.,
-        'B': torch.tensor([1., 0., 1., 1.])/3.,
-        'V': torch.tensor([1., 1., 0., 1.])/3.,
-        'D': torch.tensor([1., 1., 1., 0.])/3.,
-        'N': torch.tensor([1., 1., 1., 1.])/4.,
+        '-': torch.tensor([1., 0., 0., 0., 0., 0.]),
+        'G': torch.tensor([0., 1., 0., 0., 0., 0.]),
+        'A': torch.tensor([0., 0., 1., 0., 0., 0.]),
+        'U': torch.tensor([0., 0., 0., 1., 0., 0.]),
+        'C': torch.tensor([0., 0., 0., 0., 1., 0.]),
+        'R': torch.tensor([0., 1., 1., 0., 0., 0.])/2.,
+        'Y': torch.tensor([0., 0., 0., 1., 1., 0.])/2.,
+        'W': torch.tensor([0., 0., 1., 1., 0., 0.])/2.,
+        'S': torch.tensor([0., 1., 0., 0., 1., 0.])/2.,
+        'M': torch.tensor([0., 0., 1., 0., 1., 0.])/2.,
+        'K': torch.tensor([0., 1., 0., 1., 0., 0.])/2.,
+        'H': torch.tensor([0., 0., 1., 1., 1., 0.])/3.,
+        'B': torch.tensor([0., 1., 0., 1., 1., 0.])/3.,
+        'V': torch.tensor([0., 1., 1., 0., 1., 0.])/3.,
+        'D': torch.tensor([0., 1., 1., 1., 0., 0.])/3.,
+        'N': torch.tensor([0., 1., 1., 1., 1., 0.])/4.,
         }
 
+# TODO more efficient positional encoding?
 # TODO reorganize
-# TODO optimize
-def collate_msas(msas):
-    B = len(msas)
-    S = max([len(msa) for msa in msas])
-    L = max([msa.get_alignment_length() for msa in msas])
-    D = 4
+# TODO double check
+def collate_msas_explicit_position(msas):
+    seqlens = [len(msa[0]) for msa in msas]
+    maxlen = 1000
+    B = len(msas) # batch size
+    S = max(len(msa) for msa in msas) # number of sequences in alignment
+    L = max(seqlens) # length of sequences in alignment
+    D = 7 # gap + 4 letters + mask token + 2 dims for position
 
-    batch = torch.zeros((S, L, B, D), dtype=torch.float)
+    peindex = torch.arange(0, L)
+
+    batch = torch.zeros((B, S, L, D), dtype=torch.float)
 
     for i, msa in enumerate(msas):
         for s in range(len(msa)):
-            for l in range(msa.get_alignment_length()):
-                batch[s, l, i, ...] = rna_to_tensor_dict[msa[s, l]][...]
+            for l in range(len(msa[0])):
+                batch[i, s, l, 0:5] = rna_to_tensor_dict[msa[s, l]][:]
+                batch[i, s, l, 5] = peindex[s] / maxlen
+                batch[i, s, l, 6] = peindex[s] / len(msa[s])
 
-    batch = batch.reshape(S*L, B, D)
-    return (batch, torch.Tensor(batch))
+    return batch
