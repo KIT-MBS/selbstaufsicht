40747b07a2d85a0827dc1e96db0a388718fd282e
Oskar Taubert <oskar.taubert@kit.edu>
2021-05-05 10:17:40 +0200

    updated train script to new util interface

--- scripts/train.py
+++ scripts/train.py
@@ -1,20 +1,17 @@
 import os
-import torch
-from torch import nn
 from torch.utils.data import DataLoader
-from torch.optim import Adam
 
 from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning import Trainer
 
 from selbstaufsicht import models
 from selbstaufsicht import datasets
-from selbstaufsicht.utils import collate_msas
+from selbstaufsicht.utils import collate_msas_explicit_position
 
 
 root = os.environ['DATA_PATH']
-model = models.self_supervised.msa.MSAModel()
+model = models.self_supervised.MSAModel()
 dataset = datasets.Xfam(root, download=True)
-dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_msas, num_workers=8)
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_msas_explicit_position, num_workers=8)
 trainer = Trainer()
 trainer.fit(model, dataloader)
