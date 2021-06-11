61c248d93da22d8bed073fbe5d8cd695db7cf1c9
Oskar Taubert <oskar.taubert@kit.edu>
2021-06-11 20:54:55 +0200

    fleshed out model training some more

--- selbstaufsicht/models/self_supervised/msa/msa_module.py
+++ selbstaufsicht/models/self_supervised/msa/msa_module.py
@@ -7,14 +7,12 @@ from axial_attention import AxialAttention
 from . import modules
 
 
-# TODO do positional encoding after task preprocessing
 class MSAModel(pl.LightningModule):
     """
     Model for pre-training on multiple sequence alignments of biological sequences
     """
     def __init__(self, molecule='RNA', mask_width=1, shuffle_partitions=2, depth=4, heads=4, dim=32):
-        # TODO task parameters
-        # TODO model parameters
+        # TODO task and model parameters
         super().__init__()
 
         if molecule != 'RNA':
@@ -22,15 +20,15 @@ class MSAModel(pl.LightningModule):
 
         self.mask_width = mask_width
 
-        # self.backbone = modules.TransformerEncoderStack(4, 32, 4, 128, 12)
+        # TODO replace axial transformer with self built optimized module, to get rid of all the unncecessary permutations
         self.backbone = AxialTransformerEncoder(dim, depth=depth, heads=heads)
         self.demasking_head = modules.DemaskingHead(dim, 5)
-        # deshuffling_head = modules.DeshufflingHead(dim, 2)
-        # contrastive_head = modules.ContrastiveHead()
-        # self.heads = [demasking_head, deshuffling_head, contrastive_head]
+        self.deshuffling_head = modules.DeshufflingHead(dim, 2)
+        self.contrastive_head = modules.ContrastiveHead()
 
-        # self.criteria = [nn.KLDivLoss(reduction='batchmean'), nn.CrossEntropyLoss()]
-        self.criteria = [nn.KLDivLoss(reduction='batchmean')]
+        self.demasking_loss = nn.KLDivLoss(reduction='batchmean')
+        self.deshuffling_loss = nn.CrossEntropyLoss()
+        self.contrastive_loss = nn.CosineEmbeddingLoss()
 
     def forward(self, encoded_msa_batch):
         """
@@ -39,54 +37,49 @@ class MSAModel(pl.LightningModule):
             encoded_msa_batch: batch of msas encoded into a tensor of size [batch_size, number_of_sequences, sequence_length, embed_dim]
         """
         latent = self.backbone(encoded_msa_batch)
-
         return latent
 
-    # TODO
-    # def shared_step(self, batch_data):
-    #     latent = self(x)
-    #     demasking_result = self.demasking_head(x) # [B, S, L, 5]
-    #     deshuffling_result = self.deshuffling_head(x) # [B]
-    #     return demasking_result, deshuffling_result
-
     def training_step(self, batch_data, batch_idx):
+        # TODO move as much as possible into the collator
         batch_input, lens = batch_data
-        # batch_input, targets = self.task_transforms(batch_input)
-        mask_start = torch.randint(batch_input.size(2) - self.mask_width, size=(1,))
-        # masked_input, inpainting_mask = mask(batch_input)
-        original = batch_input.clone().detach()
-        batch_input[:, :, mask_start:mask_start + self.mask_width, 0:6] = torch.tensor([0., 0., 0., 0., 0., 1.])
-        demasking_target = original[:, :, mask_start:mask_start + self.mask_width, 0:5]
+        batch_input = batch_input[:, torch.randperm(batch_input.size(1)), :, :]
 
-        latent = self(batch_input)
+        mask_start = torch.randint(batch_input.size(2) - self.mask_width, size=(1,))
+        original = batch_input.clone().detach()
+
+        n_sequences = min(self.n_sequences, batch_input.size(1) // 2)
+        sequences_overlap = n_sequences // 10
+        in2_sequences_start = n_sequences - sequences_overlap
+        in2_sequences_end = min(batch_input.size(1), in2_sequences_start + n_sequences // 2)
+        input1 = batch_input[:, :n_sequences]
+        input2 = batch_input[:, in2_sequences_start, in2_sequences_end]
+
+        input1[:, :, mask_start:mask_start + self.mask_width, 0:6] = torch.tensor([0., 0., 0., 0., 0., 1.])
+
+        demasking_target = original[:, :, mask_start:mask_start + self.mask_width, 0:5]
+        deshuffling_target = torch.randint(self.n_partitions, batch_input.size(0))
+        input1 = jigsaw(input1, deshuffling_target)
+
+        latent = self(input1)
 
         demasking_output = self.demasking_head(latent)[:, :, mask_start:mask_start + self.mask_width, 0:5]
-        # loss = self.criteria[0](outputs, targets)
+        deshuffling_output = self.deshuffling_head(latent)
+
+        demasking_loss = self.demasking_loss(demasking_output, demasking_target)
+        deshuffling_loss = self.deshuffling_loss(deshuffling_output, deshuffling_target)
+        contrastive_loss = self.contrastive_loss(latent, self(input2))
+
+        loss = demasking_loss + deshuffling_loss + contrastive_loss
         loss = self.criteria[0](demasking_output, demasking_target)
         self.log('loss', loss)
         return loss
 
-    # def heads(self, latent, inpainting_mask):
-    #     return (self.demasking_head(latent, inpainting_mask), self.deshuffling_head(latent))
-
-    # def compute_head_output(self, latent):
-    #     return [h(latent) for h in self.heads]
-
-    # def compute_head_loss(self, outputs, targets):
-    #     # TODO log component losses
-    #     trips = zip(outputs, targets, self.criteria)
-    #     return sum([l(o, t) for (o, t, l) in trips])
-
-    # def task_transforms(self, batch_input):
-    #     # TODO modularize
-    #     demasking_target = torch.Tensor(batch_input)
-    #     masked_input, inpainting_mask = mask(batch_input)
-    #     demasking_target = demasking_target[:, :, demasking_target[0]:demasking_target[0]+demasking_target[1]]
-    #     assert demasking_target.size() == (batch_input.size(0), batch_input.size(1), self.mask_width, 8)
-    #     return masked_input, (demasking_target), (inpainting_mask)
-
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters())
+
+
+def jigsaw(deshuffling_input):
+    return
 
 # def mask(batch_data):
 #     width = self.mask_width
@@ -106,7 +99,7 @@ class AxialTransformerEncoder(nn.Module):
     def __init__(self, dim, depth, heads, dim_heads=None, dim_ff=None, pos_emb=None):
         super().__init__()
         if dim_ff is None:
-            dim_ff = 2*dim
+            dim_ff = 2 * dim
         self.pos_emb = pos_emb
         self.embedding = nn.Sequential(nn.Linear(8, dim), nn.LeakyReLU())
 
