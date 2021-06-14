bdb9b8bd57ddc2598830e64dac57c19cc6733747
Oskar Taubert <oskar.taubert@kit.edu>
2021-06-14 15:18:49 +0200

    updated imports, model parameters

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,6 @@
 import os
 from torch.utils.data import DataLoader
 
-from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning import Trainer
 
 from selbstaufsicht import models
@@ -10,7 +9,7 @@ from selbstaufsicht.utils import collate_msas_explicit_position
 
 
 root = os.environ['DATA_PATH']
-model = models.self_supervised.MSAModel()
+model = models.self_supervised.MSAModel(n_sequences=100)
 dataset = datasets.Xfam(root, download=True)
 dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_msas_explicit_position, num_workers=2)
 trainer = Trainer(gpus=1, max_epochs=5)
