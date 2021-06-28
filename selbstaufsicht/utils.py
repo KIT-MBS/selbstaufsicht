7a8712c95548b88661dedbaeffe649ccb384575d
Philipp Knechtges <philipp-dev@knechtges.com>
2021-06-28 16:44:25 +0200

    add lehmer encoding for permutations

--- selbstaufsicht/utils.py
+++ selbstaufsicht/utils.py
@@ -49,3 +49,39 @@ def collate_msas_explicit_position(msas):
                 batch[i, s, l, 7] = peindex[l] / len(msa[s])
 
     return (batch, seqlens)
+
+def lehmer_encode(i, n):
+    """
+    Encodes an integer i in the interval [0,n!-1] as a permutation of (0,1,2,...,n-1).
+
+    Example:
+        >>> lehmer_encode(10,7)
+        tensor([0, 1, 2, 4, 6, 3, 5], dtype=torch.int32)
+    """
+    pos = torch.empty((n-1,),dtype=torch.int32)
+    for j in range(2,n+1):
+        ii = i // j
+        pos[n-j] = i % j
+        i = ii
+
+    assert(i == 0)
+
+    init = torch.arange(n,dtype=torch.int32)
+    ret = torch.empty((n,),dtype=torch.int32)
+    for j in range(n-1):
+        jj = 0
+        for k in range(n):
+            if init[k] == -1:
+                continue
+            if jj == pos[j]:
+                ret[j] = k # should be the same as init[k]
+                init[k] = -1
+                break
+            jj += 1
+
+    for k in range(n):
+        if init[k] == -1:
+            continue
+        ret[n-1] = k
+
+    return ret
