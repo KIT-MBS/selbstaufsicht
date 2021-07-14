9d89a3f9fb0354ea4822c6697fa36c30b99c9b1c
Oskar Taubert <oskar.taubert@kit.edu>
2021-07-15 00:09:07 +0200

    added layer norms to attention modules

--- selbstaufsicht/modules/attention.py
+++ selbstaufsicht/modules/attention.py
@@ -2,10 +2,12 @@ import torch
 from torch import nn
 
 
-# TODO all factor kwargs
+# TODO all factory kwargs
 # TODO key padding masks
+# TODO dropout
 class MultiHeadSelfAttention2d(nn.Module):
-    def __init__(self, num_heads, dim_head, dropout=0.):
+    def __init__(self, num_heads, dim_head, dropout=0., layer_norm_eps=1e-5, device=None, dtype=None):
+        factory_kwargs = {'device': device, 'dtype': dtype}
         super(MultiHeadSelfAttention2d, self).__init__()
         self.num_heads = num_heads
         self.dim_head = dim_head
@@ -14,9 +16,9 @@ class MultiHeadSelfAttention2d(nn.Module):
             raise NotImplementedError()
         self.embed_dim = self.dim_head * self.num_heads
 
-        self.in_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1)
+        self.in_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1, **factory_kwargs)
+        self.norm = nn.LaynerNorm(dim_head * num_heads, eps=layer_norm_eps, **factory_kwargs)
 
-    # TODO dropout
     def forward(self, x, key_padding_mask=None, need_weights=True):
         """
         x: Tensor, batch first, channel first: [B, D, H, W]
@@ -42,10 +44,10 @@ class MultiHeadSelfAttention2d(nn.Module):
 
 
 class AxialSelfAttention2d(nn.Module):
-    # TODO norm
     # TODO dropout
     # TODO optimize einsum strings using opt_einsum package
-    def __init__(self, num_heads, dim_head, dropout=0.):
+    def __init__(self, num_heads, dim_head, dropout=0., layer_norm_eps=1e-5, device=None, dtype=None):
+        factory_kwargs = {'device': device, 'dtype': dtype}
         super(AxialSelfAttention2d, self).__init__()
         self.num_heads = num_heads
         self.dim_head = dim_head
@@ -55,6 +57,8 @@ class AxialSelfAttention2d(nn.Module):
         self.embed_dim = self.dim_head * self.num_heads
         self.in_row_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1)
         self.in_col_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1)
+        self.norm1 = nn.LayerNorm(self.embed_dim, eps=layer_norm_eps, **factory_kwargs)
+        self.norm2 = nn.LayerNorm(self.embed_dim, eps=layer_norm_eps, **factory_kwargs)
 
     def forward(self, x, key_padding_mask=None, need_weights=True):
         """
@@ -64,7 +68,6 @@ class AxialSelfAttention2d(nn.Module):
         assert D == self.embed_dim
         # TODO padding mask
 
-        # TODO x = self.norm(x)
         q, k, v = self.in_row_projection(x).chunk(3, dim=1)  # [B, D, S, L]
         q = q.view(B, self.num_heads, self.dim_head, S, L)  # [B, H, DH, S, L]
         k = k.view(B, self.num_heads, self.dim_head, S, L)
@@ -76,8 +79,8 @@ class AxialSelfAttention2d(nn.Module):
         row_out = torch.einsum('bhsij, bhdsj->bhdsi', row_attn, v)
         row_out = row_out.view(B, D, S, L)
         out = x + row_out
+        out = self.norm1(out)
 
-        # TODO x = self.norm(x)
         q, k, v = self.in_col_projection(out).chunk(3, dim=1)  # [B, D, S, L]
         q = q.view(B, self.num_heads, self.dim_head, S, L)  # [B, H, DH, S, L]
         k = k.view(B, self.num_heads, self.dim_head, S, L)
@@ -90,12 +93,14 @@ class AxialSelfAttention2d(nn.Module):
         col_out = col_out.view(B, D, S, L)
 
         out = out + col_out
+        out = self.norm2(out)
 
         return out
 
 
 class TiedAxialSelfAttention2d(nn.Module):
-    def __init__(self, num_heads, dim_head, dropout=0.):
+    def __init__(self, num_heads, dim_head, dropout=0., layer_norm_eps=1e-5, device=None, dtype=None):
+        factory_kwargs = {'device': device, 'dtype': dtype}
         super(TiedAxialSelfAttention2d, self).__init__()
         self.num_heads = num_heads
         self.dim_head = dim_head
@@ -103,8 +108,8 @@ class TiedAxialSelfAttention2d(nn.Module):
         if dropout > 0.:
             raise NotImplementedError()
         self.embed_dim = self.dim_head * self.num_heads
-        self.in_row_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1)
-        self.in_col_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1)
+        self.in_row_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1, **factory_kwargs)
+        self.in_col_projection = nn.Conv2d(self.embed_dim, 3 * self.embed_dim, kernel_size=1, **factory_kwargs)
 
     def forward(self, x, key_padding_mask=None, need_weights=True):
         """
@@ -113,35 +118,34 @@ class TiedAxialSelfAttention2d(nn.Module):
         B, D, S, L = x.size()
         assert D == self.embed_dim
         # TODO padding mask
-        # TODO different in_proj for each axis?
         q, k, v = self.in_row_projection(x).chunk(3, dim=1)  # [B, D, S, L]
         q = q.view(B, self.num_heads, self.dim_head, S, L)  # [B, H, DH, S, L]
         k = k.view(B, self.num_heads, self.dim_head, S, L)
         v = v.view(B, self.num_heads, self.dim_head, S, L)
 
         # NOTE row attn
-        # TODO x = self.norm(x)
         row_attn = torch.einsum('bhcsi, bhcsj->bhij', q, k)
         row_attn = row_attn.unsqueeze(2)
         row_attn = row_attn.softmax(dim=-1)  # [B, H, 1, L, L]
         row_out = torch.einsum('bhsij, bhdsj->bhdsi', row_attn, v)
         row_out = row_out.view(B, D, S, L)
-        out = x + row_out
 
-        # TODO x = self.norm(x)
+        out = x + row_out
+        out = self.norm1(out)
+
         q, k, v = self.in_col_projection(out).chunk(3, dim=1)  # [B, D, S, L]
         q = q.view(B, self.num_heads, self.dim_head, S, L)  # [B, H, DH, S, L]
         k = k.view(B, self.num_heads, self.dim_head, S, L)
         v = v.view(B, self.num_heads, self.dim_head, S, L)
 
         # NOTE col attn
-        # TODO x = self.norm(x)
         col_attn = torch.einsum('bhcil, bhcjl->bhijl', q, k)
         col_attn = col_attn.softmax(dim=-2)
         col_out = torch.einsum('bhijl, bhdjl->bhdil', col_attn, v)
         col_out = col_out.view(B, D, S, L)
 
         out = out + col_out
+        out = self.norm2(out)
 
         return out
 
