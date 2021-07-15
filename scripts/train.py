35ca271ae94e4670a1018d10a1b1c642eeb10d14
Oskar Taubert <oskar.taubert@kit.edu>
2021-07-15 12:03:12 +0200

    msa transformer hyperparams for reference

--- scripts/train.py
+++ scripts/train.py
@@ -9,8 +9,9 @@ from selbstaufsicht.utils import collate_msas_explicit_position
 
 
 root = os.environ['DATA_PATH']
-model = models.self_supervised.MSAModel(depth=2, heads=2, dim=16, n_sequences=100)
+# NOTE MSA transformer: num_layers=12, d=768, num_heads=12, batch_size=512, lr=10**-4, **-2 lr schedule, 32 V100 GPUs for 100k updates, finetune for 25k more
+model = models.self_supervised.MSAModel(depth=8, heads=4, dim=32, n_sequences=10)
 dataset = datasets.Xfam(root, download=True)
 dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_msas_explicit_position, num_workers=2)
-trainer = Trainer(gpus=1, max_epochs=1)
+trainer = Trainer(max_epochs=1)
 trainer.fit(model, dataloader)
