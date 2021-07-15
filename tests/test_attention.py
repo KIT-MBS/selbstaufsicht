46185688bed56eea5635bd4440060cf209b92e09
Oskar Taubert <oskar.taubert@kit.edu>
2021-07-15 10:53:12 +0200

    cosmetic changes

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -16,8 +16,8 @@ def test_MultiHeadAttention2d():
     x = torch.rand(bs, embed_dim, S, L)
 
     key_padding_mask = None
-    in_proj_weight = module.in_projection.weight.view(3*embed_dim, embed_dim)
-    in_proj_bias = module.in_projection.bias.view(3*embed_dim)
+    in_proj_weight = module.in_projection.weight.view(3 * embed_dim, embed_dim)
+    in_proj_bias = module.in_projection.bias.view(3 * embed_dim)
     bias_k = None
     bias_v = None
     add_zero_attn = False
@@ -27,18 +27,18 @@ def test_MultiHeadAttention2d():
 
     pred, attn = module(x)
 
-    x_ref = x.reshape(bs, embed_dim, S*L)
+    x_ref = x.reshape(bs, embed_dim, S * L)
     x_ref = x_ref.permute(2, 0, 1)
     ref, ref_attn = multi_head_attention_forward(
-            x_ref, x_ref, x_ref,
-            embed_dim, num_heads, in_proj_weight, in_proj_bias,
-            bias_k, bias_v, add_zero_attn, dropout, out_proj_weight, out_proj_bias,
-            need_weights=True, key_padding_mask=key_padding_mask, attn_mask=attn_mask)
+        x_ref, x_ref, x_ref,
+        embed_dim, num_heads, in_proj_weight, in_proj_bias,
+        bias_k, bias_v, add_zero_attn, dropout, out_proj_weight, out_proj_bias,
+        need_weights=True, key_padding_mask=key_padding_mask, attn_mask=attn_mask)
 
     pred = pred.permute(2, 3, 0, 1).reshape_as(ref)
 
     assert torch.allclose(pred, ref)
-    assert torch.allclose(attn.sum(dim=1)/num_heads, ref_attn)
+    assert torch.allclose(attn.sum(dim=1) / num_heads, ref_attn)
 
 
 def test_AxialAttention2d():
@@ -48,14 +48,14 @@ def test_AxialAttention2d():
 # NOTE mostly done as exercise/affirmation
 def test_linconfconsistency():
     bs, h, w = 1, 3, 3
-    din = 4
-    dout = 3*din
-    xconv = torch.arange(0, bs*din*h*w, dtype=torch.float).reshape(bs, din, w, h)
+    dim = 4
+    dout = 3 * dim
+    xconv = torch.arange(0, bs * dim * h * w, dtype=torch.float).reshape(bs, dim, w, h)
     xlin = xconv.permute(0, 2, 3, 1)
-    weight = torch.rand(dout, din)
+    weight = torch.rand(dout, dim)
     bias = torch.rand(dout)
     lres = torch.nn.functional.linear(xlin, weight, bias)
-    cres = torch.nn.functional.conv2d(xconv, weight.view(dout, din, 1, 1), bias)
+    cres = torch.nn.functional.conv2d(xconv, weight.view(dout, dim, 1, 1), bias)
 
     assert torch.allclose(cres, lres.permute(0, 3, 1, 2))
 
