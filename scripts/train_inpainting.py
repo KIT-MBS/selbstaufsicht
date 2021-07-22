b0ff22707ea87a23b9ca56ad33cee8c4d614029e
Oskar Taubert <oskar.taubert@kit.edu>
2021-07-22 18:12:10 +0200

    added preprocessing to inpainting training script

--- scripts/train_inpainting.py
+++ scripts/train_inpainting.py
@@ -6,6 +6,9 @@ from pytorch_lightning import Trainer
 
 from selbstaufsicht import models
 from selbstaufsicht import datasets
+from selbstaufsicht import transforms
+from selbstaufsicht.utils import rna2index
+from selbstaufsicht.models.self_supervised.transforms import MSA2Tensor, RandomMSAColumnMasking
 
 # NOTE MSA transformer reference: num_layers=12, d=768, num_heads=12, batch_size=512, lr=10**-4, **-2 lr schedule, 32 V100 GPUs for 100k updates, finetune for 25k more
 
@@ -18,7 +21,7 @@ epochs = 1
 
 root = os.environ['DATA_PATH'] + 'Xfam'
 model = models.self_supervised.MSAModel()
-transform = None
+transform = transforms.Compose([MSA2Tensor(rna2index), RandomMSAColumnMasking(p=0.15)])
 ds = datasets.Xfam(root, download=True, transform=transform)
 dl = DataLoader(ds, batch_size=batch_size, shuffle=True)
 trainer = Trainer(max_epochs=epochs)
