89cf4e5ae76b3af594c9b923d531ca3e0cf38d1a
Oskar Taubert <oskar.taubert@kit.edu>
2021-07-22 18:11:34 +0200

    added common transforms file

---
+++ selbstaufsicht/transforms.py
@@ -0,0 +1,21 @@
+class Compose:
+    """
+    Composes several transforms together.
+    For now functionally identical to torchvision.transforms.Compse.
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, sample):
+        for t in self.transforms:
+            sample = t.sample
+        return sample
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.transforms:
+            format_string += '\n'
+            format_string += '    {0}'.format(t)
+        format_string += '\n)'
+        return format_string
