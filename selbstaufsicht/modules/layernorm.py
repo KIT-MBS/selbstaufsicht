08571aef9fb10927a34dbb235b2bcd51eaea1b8b
Oskar Taubert <oskar.taubert@kit.edu>
2021-08-17 18:55:38 +0200

    cosmetics

--- selbstaufsicht/modules/layernorm.py
+++ selbstaufsicht/modules/layernorm.py
@@ -1,20 +1,21 @@
 import torch
 
+
 class AxialLayerNorm(torch.nn.Module):
-    def __init__(self, axis, axis_length, eps=1e-5, elementwise_affine = True, device=None, dtype=None):
+    def __init__(self, axis, axis_length, eps=1e-5, elementwise_affine=True, device=None, dtype=None):
         factory_kwargs = {'device': device, 'dtype': dtype}
-        super(AxialLayerNorm,self).__init__()
+        super(AxialLayerNorm, self).__init__()
 
         self.axis = axis
         self.axis_length = axis_length
         self.eps = eps
         self.elementwise_affine = elementwise_affine
         if elementwise_affine:
-            self.weight = torch.nn.Parameter(torch.empty((self.axis_length,),**factory_kwargs))
-            self.bias = torch.nn.Parameter(torch.empty((self.axis_length,),**factory_kwargs))
+            self.weight = torch.nn.Parameter(torch.empty((self.axis_length,), **factory_kwargs))
+            self.bias = torch.nn.Parameter(torch.empty((self.axis_length,), **factory_kwargs))
         else:
-            self.register_parameter("weight",None)
-            self.register_parameter("bias",None)
+            self.register_parameter("weight", None)
+            self.register_parameter("bias", None)
 
         self.reset_parameters()
 
@@ -27,4 +28,3 @@ class AxialLayerNorm(torch.nn.Module):
         tmp = torch.swapaxes(input, self.axis, -1)
         tmp2 = torch.nn.functional.layer_norm(tmp, (self.axis_length,), self.weight, self.bias, self.eps)
         return torch.swapaxes(tmp2, self.axis, -1)
-
