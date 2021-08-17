00ef10e1c4769eefd19a05f8943bb7452bd05078
Oskar Taubert <oskar.taubert@kit.edu>
2021-08-17 18:59:53 +0200

    training script done for now, more metrics later

--- scripts/train_inpainting.py
+++ scripts/train_inpainting.py
@@ -6,14 +6,14 @@ from pytorch_lightning import Trainer
 
 from selbstaufsicht import models
 from selbstaufsicht import datasets
-from selbstaufsicht import transforms
-from selbstaufsicht.utils import rna2index
-from selbstaufsicht.models.self_supervised.transforms import MSA2Tensor, RandomMSAColumnMasking
+from selbstaufsicht.models.self_supervised.msa.utils import get_tasks
+from selbstaufsicht.utils import pad_collate_fn
+from selbstaufsicht.utils import rna2index as token_dict
 
 # training parameters
-batch_size = 1
 epochs = 1
-batch_size = 512
+# NOTE single GPU for now
+batch_size = 512 // 32
 lr = 0.0001
 warmup = 16000
 # TODO implement msa subsampling random, hamming maximizing
@@ -23,12 +23,15 @@ msa_sampling = 'random'
 num_layers = 12
 d = 768
 num_heads = 12
+d_head = d//num_heads
+tasks = ['inpainting']
 
+
+transform, heads = get_tasks(tasks, d, token_dict, subsampling=msa_sampling, masking_strategy='token')
 
 root = os.environ['DATA_PATH'] + 'Xfam'
-model = models.self_supervised.MSAModel()
-transform = transforms.Compose([MSA2Tensor(rna2index), RandomMSAColumnMasking(p=0.15)])
 ds = datasets.Xfam(root, download=True, transform=transform)
-dl = DataLoader(ds, batch_size=batch_size, shuffle=True)
+model = models.self_supervised.MSAModel(num_layers, num_heads, d, heads=heads)
+dl = DataLoader(ds, batch_size=batch_size, collate_fn=pad_collate_fn, shuffle=True)
 trainer = Trainer(max_epochs=epochs)
 trainer.fit(model, dl)
