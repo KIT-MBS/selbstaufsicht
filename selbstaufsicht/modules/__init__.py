9f7208514bd1c4a9e37dbad131099c3103ee950e
Oskar Taubert <oskar.taubert@kit.edu>
2021-08-17 16:04:25 +0200

    updated modules init

--- selbstaufsicht/modules/__init__.py
+++ selbstaufsicht/modules/__init__.py
@@ -1,3 +1,5 @@
 from .attention import MultiHeadSelfAttention2d
 from .attention import AxialSelfAttention2d
 from .attention import TiedAxialSelfAttention2d
+from .attention import Transmorpher2d
+from .attention import TransmorpherLayer2d
