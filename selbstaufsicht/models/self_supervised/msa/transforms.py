d12816313229c9b028dd7184b5d66aef286c1f5d
Oskar Taubert <oskar.taubert@kit.edu>
2021-08-24 14:10:23 +0200

    preprocessing transform consistency

--- selbstaufsicht/models/self_supervised/msa/transforms.py
+++ selbstaufsicht/models/self_supervised/msa/transforms.py
@@ -1,93 +1,53 @@
-import numpy as np
 import torch
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 
 
-class MSADataTrainTransform(object):
-    """
-    Transforms for multiple sequence alignment data
-
-    Transform::
-        String of RNA letters to token indices
-    """
-
-    def __init__(self, masking=True, jigsaw=True, contrastive=True, ambiguity=None):
-        return
+class MSATokenizeOneHot():
+    def __init__(self, mapping):
+        self.mapping = mapping
 
     def __call__(self, msa):
-        # return [[rna_to_index[a] for a in sequence] for sequence in msa]
-        pass
-
-        # return (sample, target)
+        torch.nn.functional.one_hot(torch.tensor([[self.mapping[letter] for letter in sequence] for sequence in msa], dtype=torch.float))
 
 
-# TODO
-class RandomMSAColumnMasking(object):
-    def __init__(self, p):
+class RandomMSAMasking():
+    def __init__(self, p, mode, mask_token):
         self.p = p
+        self.mask_token = mask_token
+        self.masking_fn = _get_masking_fn(mode)
 
-    def __call__(self, msa_tensor):
-        raise
-        return
-
-
-# TODO
-class RandomMSATokenMasking(object):
-    def __init__(self, p):
-        self.p = p
-
-    def __call__(self, msa_tensor):
-        raise
-        return
+    def __call__(self, x):
+        return self.masking_fn(x, self.p, self.mask_token)
 
 
-class MSAJigsawTransform(object):
-    def __init__(self, npartitions=2, minfringe=5, ):
-        return
+class ExplicitPositionalEncoding():
+    def __init__(self, axis=-1):
+        self.axis = axis
+
+    def __call__(self, x):
+        size = x.size(self.axis)
+        absolute = torch.arange(0, size, dtype=torch.float)
+        relative = absolute/size
+
+        return {'msa': x, 'sequential_features': torch.cat((absolute, relative), dim=self.axis)}
 
 
-class MSAContrastiveTransform(object):
-    def __init__(self):
-        return
-
-
-# class MSA2Tensor(object):
-#     def __init__(self, mapping, device=None):
-#         self.mapping = mapping
-#         self.device = device
-#         return
+# class OneHotMSAArray(object):
+#     def __init__(self, mapping):
+#         maxind = 256
 #
-#     def __call__(self, msa):
-#         return torch.tensor([[self.mapping[letter] for letter in sequence] for sequence in msa], device=self.device)
+#         self.mapping = np.full((maxind, ), -1)
+#         for k in mapping:
+#             self.mapping[ord(k)] = mapping[k]
 #
+#     def __call__(self, msa_array):
+#         """
+#         msa_array: byte array
+#         """
 #
-# class MSA2array(object):
-#     '''
-#     takes an MSA object and produces a numpy char array
-#     '''
-#     def __init__(self):
-#         return
-#
-#     def __call__(self, msa):
-#         return np.array([list(rec) for rec in msa], dtype='<U1')
-
-
-class OneHotMSAArray(object):
-    def __init__(self, mapping):
-        maxind = 256
-
-        self.mapping = np.full((maxind, ), -1)
-        for k in mapping:
-            self.mapping[ord(k)] = mapping[k]
-
-    def __call__(self, msa_array):
-        """
-        msa_array: byte array
-        """
-
-        return self.mapping[msa_array.view(np.uint32)]
+#         return self.mapping[msa_array.view(np.uint32)]
 
 
 def _jigsaw(msa, permutation, minleader=0, mintrailer=0, delimiter_token='|'):
@@ -114,10 +74,14 @@ def _jigsaw(msa, permutation, minleader=0, mintrailer=0, delimiter_token='|'):
 
 
 # TODO adapt to tensorized input
-def _block_mask_msa(msa, begin, end, mask_token='*'):
+def _block_mask_msa(msa, p, mask_token='*'):
     """
     masks out a contiguous block of columns in the given msa
     """
+    total_length = msa.size(-1)
+    mask_length = int(total_length * p)
+    begin = torch.randint(total_length-mask_length, (1)).item()
+    end = begin + mask_length
     masked = msa[:, begin:end]
     mask = MultipleSeqAlignment([SeqRecord(Seq(mask_token * (end - begin)), id=r.id) for r in msa])
     msa = msa[:, :begin] + mask + msa[:, end:]
@@ -144,8 +108,22 @@ def _token_mask_msa(msa_tensor, coords, mask_token_index):
     return msa_tensor, masked
 
 
+def _get_masking_fn(mode):
+    if mode == 'token':
+        return _token_mask_msa
+    elif mode == 'column':
+        return _column_mask_msa
+    elif mode == 'block':
+        return _block_mask_msa
+    raise ValueError('unknown token masking mode', mode)
+
+
 def _subsample_uniform(msa, contrastive=False):
     # TODO
+    raise
+
+
+def _get_msa_subsampling_fn(mode):
     raise
 
 
