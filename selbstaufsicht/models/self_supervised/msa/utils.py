4bf77f7dd6e426cbe240c076d5f3453709f8da6a
Oskar Taubert <oskar.taubert@kit.edu>
2021-08-25 15:30:44 +0200

    updated task utils interface consistency

--- selbstaufsicht/models/self_supervised/msa/utils.py
+++ selbstaufsicht/models/self_supervised/msa/utils.py
@@ -1,24 +1,28 @@
+from torch.nn import CrossEntropyLoss
 from selbstaufsicht import transforms
 from selbstaufsicht.utils import rna2index
 from selbstaufsicht.models.self_supervised.msa.transforms import MSATokenize,  RandomMSAMasking, ExplicitPositionalEncoding
 from .modules import InpaintingHead
 
+# NOTE mask and delimiter tokens can not be reconstructed
+num_special_tokens = 2
+
 
 def get_tasks(task, dim, **kwargs):
     # TODO other tasks
-    if task != 'inpainting':
-        raise NotImplementedError
-    if kwargs['subsampling'] != 'random':
-        raise NotImplementedError
+    if task != ['inpainting']:
+        raise NotImplementedError('only inpainting')
+    if kwargs['subsampling'] != 'token':
+        raise NotImplementedError('only token masking')
 
-    masking = kwargs.get(['masking'], 'uniform')
-    p_mask = kwargs.get(['p_mask'], 0.15)
+    masking = kwargs.get('masking', 'token')
+    p_mask = kwargs.get('p_mask', 0.15)
     transform = transforms.Compose(
             [
                 MSATokenize(rna2index),
-                RandomMSAMasking(p=p_mask, mode=masking, mask_token=rna2index['MASK_TKEN']),
+                RandomMSAMasking(p=p_mask, mode=masking, mask_token=rna2index['MASK_TOKEN']),
                 ExplicitPositionalEncoding(),
             ])
-    head = InpaintingHead(dim, kwargs['num_classes'])
+    head = InpaintingHead(dim, len(rna2index)-num_special_tokens)
 
-    return transform, head
+    return transform, {'inpainting': head}, {'inpainting': CrossEntropyLoss()}
