33c47cdc956823ef9d29b04a1d1cd45536b8b9ad
Oskar Taubert <oskar.taubert@kit.edu>
2021-08-30 18:09:07 +0200

    updated script to new model interface

--- scripts/train_inpainting.py
+++ scripts/train_inpainting.py
@@ -29,13 +29,14 @@ d_head = d//num_heads
 tasks = ['inpainting']
 
 
+# TODO should take token mapping
 transform, task_heads, task_losses = get_tasks(tasks, d, subsampling=msa_sampling, masking='token')
 
 root = os.environ['DATA_PATH'] + 'Xfam'
 print('data')
 ds = datasets.Xfam(root, download=True, transform=transform)
 print('model')
-model = models.self_supervised.MSAModel(num_layers, num_heads, d_head, task_heads=task_heads)
+model = models.self_supervised.MSAModel(num_layers, num_heads, d_head, aux_input_dim=2, task_heads=task_heads, task_losses=task_losses, in_dict_size=len(ds.token_mapping), padding_token=ds.token_mapping['PADDING_TOKEN'])
 summary(model)
 # dl = DataLoader(ds, batch_size=batch_size, collate_fn=pad_collate_fn, shuffle=True)
 dl = DataLoader(ds, batch_size=batch_size, shuffle=True)
