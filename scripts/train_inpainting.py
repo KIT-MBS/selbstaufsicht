63773309bfa43f92a0fd295ee8f2c7d0cd18bd3b
Oskar Taubert <oskar.taubert@kit.edu>
2021-09-01 19:55:55 +0200

    cosmetic changes

--- scripts/train_inpainting.py
+++ scripts/train_inpainting.py
@@ -36,7 +36,16 @@ root = os.environ['DATA_PATH'] + 'Xfam'
 print('data')
 ds = datasets.Xfam(root, download=True, transform=transform)
 print('model')
-model = models.self_supervised.MSAModel(num_layers, num_heads, d_head, aux_input_dim=2, task_heads=task_heads, task_losses=task_losses, in_dict_size=len(ds.token_mapping), padding_token=ds.token_mapping['PADDING_TOKEN'])
+model = models.self_supervised.MSAModel(
+        num_layers,
+        num_heads,
+        d_head,
+        aux_input_dim=2,
+        task_heads=task_heads,
+        task_losses=task_losses,
+        in_dict_size=len(ds.token_mapping), padding_token=ds.token_mapping['PADDING_TOKEN']
+    )
+
 summary(model)
 # dl = DataLoader(ds, batch_size=batch_size, collate_fn=pad_collate_fn, shuffle=True)
 dl = DataLoader(ds, batch_size=batch_size, shuffle=True)
