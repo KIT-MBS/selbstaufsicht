b0f951ff64ca79e23bba9e636038ab21a95d3ce3
Oskar Taubert <oskar.taubert@kit.edu>
2021-09-01 15:16:58 +0200

    cosmetic changes

--- tests/test_layernorm.py
+++ tests/test_layernorm.py
@@ -1,30 +1,31 @@
 import torch
-from selbstaufsicht.modules.layernorm import *
+from selbstaufsicht.modules.layernorm import AxialLayerNorm
 import unittest
+
 
 class TestAxialLayerNorm(unittest.TestCase):
     def test_layernorm_simple(self):
-        tmp = (torch.arange(10)*10.0).reshape((5,2))
+        tmp = (torch.arange(10)*10.0).reshape((5, 2))
         module = AxialLayerNorm(1, 2, elementwise_affine=False)
         module2 = torch.nn.LayerNorm((2,))
         tmp2 = module(tmp)
         tmp3 = module2(tmp)
-        self.assertTrue(torch.isclose(tmp2,tmp3).all())
+        self.assertTrue(torch.isclose(tmp2, tmp3).all())
 
     def test_layernorm_nonstandardaxis(self):
-        tmp = (torch.arange(10,dtype=torch.double)*10.0).reshape((5,2))
+        tmp = (torch.arange(10, dtype=torch.double)*10.0).reshape((5, 2))
         module = AxialLayerNorm(0, 5, elementwise_affine=False)
         tmp2 = module(tmp)
-        self.assertTrue(torch.isclose(tmp2[:,0],tmp2[:,1]).all())
+        self.assertTrue(torch.isclose(tmp2[:, 0], tmp2[:, 1]).all())
 
     def test_layernorm_mean(self):
-        tmp = (torch.arange(10,dtype=torch.double)*10.0).reshape((5,2))
+        tmp = (torch.arange(10, dtype=torch.double)*10.0).reshape((5, 2))
         module = AxialLayerNorm(0, 5, elementwise_affine=False)
-        tmp2 = torch.sum(module(tmp),dim=0)
-        self.assertTrue(torch.isclose(tmp2,torch.zeros_like(tmp2)).all())
+        tmp2 = torch.sum(module(tmp), dim=0)
+        self.assertTrue(torch.isclose(tmp2, torch.zeros_like(tmp2)).all())
 
     def test_layernorm_stddev(self):
-        tmp = (torch.arange(10,dtype=torch.double)*10.0).reshape((5,2))
+        tmp = (torch.arange(10, dtype=torch.double)*10.0).reshape((5, 2))
         module = AxialLayerNorm(0, 5, elementwise_affine=False)
-        tmp2 = torch.sqrt(torch.sum(module(tmp)**2,dim=0)/tmp.shape[0])
-        self.assertTrue(torch.isclose(tmp2,torch.ones_like(tmp2)).all())
+        tmp2 = torch.sqrt(torch.sum(module(tmp)**2, dim=0)/tmp.shape[0])
+        self.assertTrue(torch.isclose(tmp2, torch.ones_like(tmp2)).all())
