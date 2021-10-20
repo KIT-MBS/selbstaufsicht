7291a63c0527941be537da77cfc1b686dcb67343
Fabrice von der Lehr <fabrice.lehr@dlr.de>
2021-10-20 10:02:23 +0200

    Added test for inpainting head

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -105,15 +105,34 @@ def test_msa_mask_block(tokenized_msa):
     testing.assert_equal(y['inpainting'], y_ref['inpainting'])
 
 
-# TODO: Complete test
 def test_inpainting_head():
-    pass
-    # model = InpaintingHead
-    # latent = torch.rand()
-    # mask = torch.tensor()
+    num_classes = 4
+    bs, S, L, D = 2, 3, 4, 8
 
-    # out = model(latent, {'mask': mask})
-    # assert out
+    model = InpaintingHead(D, num_classes)
+    latent = torch.rand((bs, S, L, D))
+    mask = torch.full((bs, S, L), 0.5)
+    mask = torch.bernoulli(mask).to(torch.bool)
+
+    out = model(latent, {'mask': mask})
+    out_ref = torch.tensor([[0.8108, 0.4546,  0.0308, -0.3818],
+                            [0.7133, 0.7049, -0.0218, -0.3852],
+                            [0.6086, 0.4439, -0.2997, -0.0855],
+                            [0.5279, 0.2488, -0.1313, -0.0970],
+                            [0.4448, 0.5628, -0.2541, -0.0900],
+                            [0.4254, 0.5415, -0.1067, -0.0165],
+                            [0.3169, 0.5469, -0.2521,  0.0763],
+                            [0.5263, 0.7497, -0.0420, -0.3318],
+                            [0.4400, 0.8195, -0.1103,  0.0479],
+                            [0.5831, 0.7132, -0.2069, -0.2395],
+                            [0.8939, 0.5034,  0.1073, -0.3953],
+                            [0.5738, 0.5081, -0.0580, -0.1092],
+                            [0.6805, 0.5888, -0.1333, -0.3499],
+                            [0.7092, 0.4774,  0.0135, -0.2209],
+                            [0.5564, 0.4920, -0.2069, -0.0589],
+                            [0.7314, 0.1561, -0.0317, -0.2996]])
+    
+    testing.assert_allclose(out, out_ref, atol=1e-4, rtol=1e-3)
 
 
 def test_subsampling(basic_msa):
