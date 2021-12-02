cdb8b9378d63978e0d0887d20c7a06ca76e0d8dd
Fabrice von der Lehr <fabrice.lehr@dlr.de>
2021-12-02 18:10:31 +0100

    Added type hints to msa_modules.py and modules.py

--- selbstaufsicht/models/self_supervised/msa/modules.py
+++ selbstaufsicht/models/self_supervised/msa/modules.py
@@ -1,10 +1,20 @@
-# import math
+from typing import Dict, Union
 import torch
 import torch.nn as nn
 
 
 class InpaintingHead(nn.Module):
-    def __init__(self, d, num_classes, device=None, dtype=None):
+    def __init__(self, d: int, num_classes: int, device: Union[str, torch.device] = None, dtype: torch.dtype = None) -> None:
+        """
+        Initializes the head module for the inpaiting upstream task.
+
+        Args:
+            d (int): Embedding dimensionality.
+            num_classes (int): Number of classes (number of predictable tokens)
+            device (Union[str, torch.device], optional): Used computation device. Defaults to None.
+            dtype (torch.dtype, optional): Used tensor dtype. Defaults to None.
+        """
+        
         factory_kwargs = {'device': device, 'dtype': dtype}
         super(InpaintingHead, self).__init__()
         # TODO assuming the last layer in an encoder block is a nonlinearity
@@ -12,7 +22,18 @@ class InpaintingHead(nn.Module):
         self.proj = nn.Linear(d, num_classes, **factory_kwargs)
 
     # NOTE the output is basically flattened (of  shape (-1, num_classes)) since the number of masked tokens per sample in the batch is not the same
-    def forward(self, latent, x):
+    def forward(self, latent: torch.Tensor, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """
+        Receives latent representation, performs linear transformation and applies inpainting mask to predict masked tokens.
+
+        Args:
+            latent (torch.Tensor): Latent representation [B, E, L, D].
+            x (Dict[str, torch.Tensor]): Input data.
+
+        Returns:
+            torch.Tensor: Inpainting prediction [B*E*L, NClasses].
+        """
+        
         # latent is of shape [B, E, L, D]
         output = self.proj(latent)  # [B, E, L, NClasses]
         output = output[x['mask']]
@@ -22,12 +43,33 @@ class InpaintingHead(nn.Module):
 
 
 class JigsawHead(nn.Module):
-    def __init__(self, d, nclasses, device=None, dtype=None):
+    def __init__(self, d: int, num_classes: int, device: Union[str, torch.device] = None, dtype: torch.dtype = None) -> None:
+        """
+        Initializes the head module for the jigsaw upstream task.
+
+        Args:
+            d (int): Embedding dimensionality.
+            num_classes (int): Number of classes (number of allowed permutations)
+            device (Union[str, torch.device], optional): Used computation device. Defaults to None.
+            dtype (torch.dtype, optional): Used tensor dtype. Defaults to None.
+        """
+        
         factory_kwargs = {'device': device, 'dtype': dtype}
         super(JigsawHead, self).__init__()
-        self.proj = nn.Linear(d, nclasses, **factory_kwargs)
+        self.proj = nn.Linear(d, num_classes, **factory_kwargs)
 
-    def forward(self, latent, x):
+    def forward(self, latent: torch.Tensor, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """
+        Receives latent representation, performs linear transformation to predict applied permutations.
+
+        Args:
+            latent (torch.Tensor): Latent representation [B, E, L, D].
+            x (Dict[str, torch.Tensor]): Input data.
+
+        Returns:
+            torch.Tensor: Jigsaw prediction [B, NClasses, E].
+        """
+        
         # latent is of shape [B, E, L, D]
         latent = latent[:, :, 0, :]  # [B, E, D]
         return torch.transpose(self.proj(latent), 1, 2)  # [B, NClasses, E]
@@ -35,7 +77,17 @@ class JigsawHead(nn.Module):
 
 # TODO different hidden and out dim?
 class ContrastiveHead(nn.Module):
-    def __init__(self, d, layer_norm_eps=1e-5, device=None, dtype=None):
+    def __init__(self, d: int, layer_norm_eps: float = 1e-5, device: Union[str, torch.device] = None, dtype: torch.dtype = None) -> None:
+        """
+        Initializes the head module for the contrastive upstream task.
+
+        Args:
+            d (int): Embedding dimensionality.
+            layer_norm_eps (float, optional): Epsilon used by LayerNormalization. Defaults to 1e-5.
+            device (Union[str, torch.device], optional): Used computation device. Defaults to None.
+            dtype (torch.dtype, optional): Used tensor dtype. Defaults to None.
+        """
+        
         factory_kwargs = {'device': device, 'dtype': dtype}
         super(ContrastiveHead, self).__init__()
         self.proj = nn.Sequential(
@@ -45,7 +97,19 @@ class ContrastiveHead(nn.Module):
             nn.Linear(d, d, bias=False, **factory_kwargs),
         )
 
-    def forward(self, latent, x):
+    def forward(self, latent: torch.Tensor, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """
+        Receives latent representation, passes mean over MSA through 2-layer-Feedforward-NN to create another latent representation,
+        which is then to be compared to that of the contrastive input.
+
+        Args:
+            latent (torch.Tensor): Latent representation [B, E, L, D].
+            x (Dict[str, torch.Tensor]): Input data.
+
+        Returns:
+            torch.Tensor: Contrastive latent representation [B, D].
+        """
+        
         # latent is of shape [B, E, L, D]
         latent = latent.mean(dim=-2)  # [B, E, D]
         latent = latent.mean(dim=-2)  # [B, D]
