2c81710a39c1d486beb5588b5deec1951e82b04a
Fabrice von der Lehr <fabrice.lehr@dlr.de>
2021-12-03 18:12:10 +0100

    Added type hints for loss.py and metrics.py

--- selbstaufsicht/modules/loss.py
+++ selbstaufsicht/modules/loss.py
@@ -3,10 +3,28 @@ from pl_bolts.losses.self_supervised_learning import nt_xent_loss
 
 
 class NT_Xent_Loss(nn.Module):
-    def __init__(self, temperature):
+    def __init__(self, temperature: float) -> None:
+        """
+        Initializes NT Xent Loss (SimCLR).
+
+        Args:
+            temperature (float): Distillation temperature.
+        """
+        
         super(NT_Xent_Loss, self).__init__()
         self.temperature = temperature
 
     # TODO ask about the distributed stuff, have to reimplement with our own solution?
-    def forward(self, x1, x2):
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        """
+        Computes NT Xent Loss.
+
+        Args:
+            x1 (torch.Tensor): First input data.
+            x2 (torch.Tensor): Second input data.
+
+        Returns:
+            torch.Tensor: Loss value.
+        """
+        
         return nt_xent_loss(x1, x2, self.temperature)
--- selbstaufsicht/modules/metrics.py
+++ selbstaufsicht/modules/metrics.py
@@ -2,12 +2,31 @@ import torch
 from torch import nn
 
 class Accuracy(nn.Module):
-    def __init__(self, class_dim=-1, ignore_index=-1):
+    def __init__(self, class_dim: int = -1, ignore_index: int = -1):
+        """
+        Initializes accuracy metric with ignore-index support and specifiable class dimension.
+
+        Args:
+            class_dim (int, optional): Class dimension, which is used for comparison. All other dimensions are treated as batch dimensions. Defaults to -1.
+            ignore_index (int, optional): Class index which is ignored in comparison. Defaults to -1.
+        """
+        
         super(Accuracy, self).__init__()
         self.class_dim = class_dim
         self.ignore_index = ignore_index
     
-    def forward(self, preds, target):
+    def forward(self, preds: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        Computes accuracy for given predictions and target data.
+
+        Args:
+            preds (torch.Tensor): Predictions.
+            target (torch.Tensor): Target data. Is expected to not contain class dim as \"preds\", but class indices.
+
+        Returns:
+            torch.Tensor: Accuracy.
+        """
+        
         # check if shapes match (exclude class dim for preds due to one-hot encoding)
         assert preds.shape[:self.class_dim] + (preds.shape[self.class_dim + 1:] if self.class_dim != -1 else ()) == target.shape, "Shapes must match except for \'class_dim\'!"
         
