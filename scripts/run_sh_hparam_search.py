3e83d9d4687f6d6d2ae76c8df058ec97c20d19bd
Fabrice von der Lehr <fabrice.lehr@dlr.de>
2021-12-20 19:38:03 +0100

    Adapted bash script runner

--- scripts/run_sh_hparam_search.py
+++ scripts/run_sh_hparam_search.py
@@ -2,9 +2,9 @@ import glob
 import os
 import subprocess
 
-# Starts bash scripts for grid-search-based hyperparameter optimization.
+# Starts bash scripts for random-search-based hyperparameter optimization.
 
-run_file_prefix = 'run_grid'
+run_file_prefix = 'run_hparam_search'
 rel_working_dir = 'profile/selbstaufsicht/scripts/'
 home = os.environ['HOME']
 
