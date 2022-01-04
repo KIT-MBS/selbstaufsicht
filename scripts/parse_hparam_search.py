a992240e4b7c3f0974ad1035f73353d78336efe2
Fabrice von der Lehr <fabrice.lehr@dlr.de>
2022-01-04 19:19:49 +0100

    Minor changes in the hparam search parsing script

--- scripts/parse_hparam_search.py
+++ scripts/parse_hparam_search.py
@@ -1,30 +1,24 @@
 import argparse
-from enum import Enum
 import glob
 import os
+import math
 import pandas as pd
 import pickle
-from tensorflow.core.util import event_pb2
-from tensorflow.data import TFRecordDataset
-import torch
+#from tensorflow.core.util import event_pb2
+#from tensorflow.data import TFRecordDataset
 
 # Parsing script for random-search-based hyperparameter optimization.
 
 # DEFINE PARSER
-parser = argparse.ArgumentParser(description='Selbstaufsicht Random-Search Hyperparameter Optimization - Parsing Script')
-parser.add_argument('--acc-threshold-inpainting-train', default=0.8, type=float, help="Minimum inpainting training accuracy for acceptance")
-parser.add_argument('--acc-threshold-inpainting-val', default=0.8, type=float, help="Minimum inpainting validation accuracy for acceptance")
-parser.add_argument('--acc-threshold-jigsaw-train', default=0.95, type=float, help="Minimum jigsaw training accuracy for acceptance")
-parser.add_argument('--acc-threshold-jigsaw-val', default=0.95, type=float, help="Minimum jigsaw validation accuracy for acceptance")
-parser.add_argument('--task-inpainting', action='store_true', help="Activates the inpainting task")
-parser.add_argument('--task-jigsaw', action='store_true', help="Activates the jigsaw task")
-parser.add_argument('--task-contrastive', action='store_true', help="Activates the contrastive task")
-parser.add_argument('--min-walltime', default=169200, type=int, help="Minimum walltime in seconds. Default: 169200s = 47h")
-parser.add_argument('--normalize', action='store_true', help="Normalizes in the acceptance range")
-parser.add_argument('--disable-baseline', action='store_true', help="Disables baseline distribution")
-parser.add_argument('--log-dir', default='lightning_logs/', type=str, help='Logging directory. Default: \"lightning_logs/\"')
-parser.add_argument('--log-exp-name', default='hparam_random_search', type=str, help='Logging experiment name. If empty, this structure level is omitted. Default: \"hparam_random_search\"')
-args = parser.parse_args()
+def init():
+    parser = argparse.ArgumentParser(description='Selbstaufsicht Random-Search Hyperparameter Optimization - Parsing Script')
+    parser.add_argument('--task-inpainting', action='store_true', help="Activates the inpainting task")
+    parser.add_argument('--task-jigsaw', action='store_true', help="Activates the jigsaw task")
+    parser.add_argument('--task-contrastive', action='store_true', help="Activates the contrastive task")
+    parser.add_argument('--min-walltime', default=169200, type=int, help="Minimum walltime in seconds. Default: 169200s = 47h")
+    parser.add_argument('--log-dir', default='lightning_logs/', type=str, help='Logging directory. Default: \"lightning_logs/\"')
+    parser.add_argument('--log-exp-name', default='hparam_random_search', type=str, help='Logging experiment name. If empty, this structure level is omitted. Default: \"hparam_random_search\"')
+    return parser.parse_args()
 
 
 # cf. https://gist.github.com/laszukdawid/62656cf7b34cac35b325ba21d46ecfcd
@@ -73,7 +67,7 @@ def convert_tb_data(root_dir, sort_by='step', df_names=[]):
     out = []
     for (root, _, filenames) in os.walk(root_dir):
         for filename in filenames:
-            if "events.out.tfevents" not in filename:
+            if 'events.out.tfevents' not in filename:
                 continue
             file_full_path = os.path.join(root, filename)
             out.append(convert_tfevent(file_full_path))
@@ -90,29 +84,29 @@ def convert_tb_data(root_dir, sort_by='step', df_names=[]):
     return all_df
 
 
-def get_task_data():
-    task_name = "t_"
+def get_task_data(args):
+    task_name = 't_'
     df_names = []
     if args.task_inpainting and not args.task_jigsaw and not args.task_contrastive:
-        task_name += "inpainting__"
-        df_names.append("inpainting_training_acc_epoch")
-        df_names.append("inpainting_validation_acc")
+        task_name += 'inpainting__'
+        df_names.append('inpainting_training_acc_epoch')
+        df_names.append('inpainting_validation_acc')
     elif not args.task_inpainting and args.task_jigsaw and not args.task_contrastive:
-        task_name += "jigsaw__"
-        df_names.append("jigsaw_training_acc_epoch")
-        df_names.append("jigsaw_validation_acc")
+        task_name += 'jigsaw__'
+        df_names.append('jigsaw_training_acc_epoch')
+        df_names.append('jigsaw_validation_acc')
     elif args.task_inpainting and args.task_jigsaw and not args.task_contrastive:
-        task_name += "inpainting+jigsaw__"
-        df_names.append("inpainting_training_acc_epoch")
-        df_names.append("inpainting_validation_acc")
-        df_names.append("jigsaw_training_acc_epoch")
-        df_names.append("jigsaw_validation_acc")
+        task_name += 'inpainting+jigsaw__'
+        df_names.append('inpainting_training_acc_epoch')
+        df_names.append('inpainting_validation_acc')
+        df_names.append('jigsaw_training_acc_epoch')
+        df_names.append('jigsaw_validation_acc')
     elif args.task_inpainting and args.task_jigsaw and args.task_contrastive:
-        task_name += "inpainting+jigsaw+contrastive__"
-        df_names.append("inpainting_training_acc_epoch")
-        df_names.append("inpainting_validation_acc")
-        df_names.append("jigsaw_training_acc_epoch")
-        df_names.append("jigsaw_validation_acc")
+        task_name += 'inpainting+jigsaw+contrastive__'
+        df_names.append('inpainting_training_acc_epoch')
+        df_names.append('inpainting_validation_acc')
+        df_names.append('jigsaw_training_acc_epoch')
+        df_names.append('jigsaw_validation_acc')
     else:
         raise ValueError("Invalid task configuration!")
     
@@ -120,7 +114,7 @@ def get_task_data():
 
 
 def parse_hparam(filename):
-    hparam_str = "nb_" + filename.split('nb_')[-1]
+    hparam_str = 'nb_' + filename.split('nb_')[-1]
     hparam_str = hparam_str.split('__rng_')[0]
     hparam_str_partitions = hparam_str.split('__')
     hparam = {}
@@ -140,13 +134,16 @@ def parse_hparam(filename):
         
 
 def parse():
-    task_name, df_names = get_task_data()
+    args = init()
+    task_name, df_names = get_task_data(args)
     root_path = os.path.join(args.log_dir, args.log_exp_name, task_name)
-    filenames = glob.glob("%s*" % root_path)
+    filenames = glob.glob('%s*' % root_path)
     num_filenames = len(filenames)
     data = {df_name: [] for df_name in df_names}
     
     for idx, filename in enumerate(filenames):
+        print("%d / %d" % (idx+1, num_filenames))
+        
         hparam = parse_hparam(filename)
         for df_name in data.keys():
             data[df_name].append(hparam.copy())
@@ -159,12 +156,12 @@ def parse():
         for name, df in all_df.items():
             first_row, last_row = df.iloc[0], df.iloc[-1]
             walltime = last_row['wall_time'] - first_row['wall_time']
-            if walltime < args.min_walltime:
-                break
-            data[name][-1]['value'] = last_row['value']
-        
-        print("%d / %d" % (idx+1, num_filenames))
+            if walltime < args.min_walltime or math.isnan(last_row['value']):
+                data[name][-1]['value'] = 0.
+            else:
+                data[name][-1]['value'] = last_row['value']
     
+    # data structure: metric -> run -> hparam
     with open('%s.pkl' % task_name, 'wb') as f:
         pickle.dump(data, f)
     
