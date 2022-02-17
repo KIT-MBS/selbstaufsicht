5967790c86d3e376b8aa0fd2aa479124141b2de2
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2022-02-17 17:25:34 +0100

    Added confusion matrix to downstream

--- selbstaufsicht/models/self_supervised/msa/msa_module.py
+++ selbstaufsicht/models/self_supervised/msa/msa_module.py
@@ -1,9 +1,11 @@
 import math
 from typing import Any, Dict, Tuple, Union
+
+import pandas as pd
+import pytorch_lightning as pl
 import torch
 from torch import nn
-
-import pytorch_lightning as pl
+from torchmetrics.functional import confusion_matrix
 
 from selbstaufsicht.modules import Transmorpher2d, TransmorpherBlock2d
 
@@ -119,7 +121,7 @@ class MSAModel(pl.LightningModule):
         x = self.embedding(x) + self.positional_embedding(aux_features)
         return self.backbone(x, padding_mask, self.need_attn, self.attn_chunk_size)
 
-    def _step(self, batch_data: Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]], batch_idx: int) -> torch.Tensor:
+    def _step(self, batch_data: Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]], batch_idx: int) -> Dict[str, Union[Dict[str, torch.Tensor], torch.Tensor]]:
         """
         Performs a single training or validation step: First passes cropped, subsampled and tokenized MSAs through the backbone model,
         whose latent representation output is then passed through the upstream task related head models.
@@ -130,7 +132,7 @@ class MSAModel(pl.LightningModule):
             batch_idx (int): Batch number.
 
         Returns:
-            torch.Tensor: Summed loss across all upstream tasks
+            Dict[str, Union[Dict[str, torch.Tensor], torch.Tensor]]: Input, target, predictions, summed loss across all upstream tasks
         """
 
         x, y = batch_data
@@ -166,8 +168,33 @@ class MSAModel(pl.LightningModule):
             self.log(f'{task}_{mode}_loss', lossvals[task], on_step=self.training, on_epoch=True)
 
         self.log(f'{mode}_loss', loss, on_step=self.training, on_epoch=True)
+        
+        out = {'input': x, 'preds': preds, 'target': y}
         if self.training:
-            return loss
+            out['loss'] = loss
+        return out
+    
+    def _epoch_end(self, outputs: List[Any]) -> None:
+        """
+        Is invoked at the end of an epoch. Computes confusion matrix, if \"contact\" is in tasks.
+
+        Args:
+            outputs (List[Any]): Outputs from training/validation steps.
+        """
+        
+        if 'contact' in self.tasks:
+            mode = "training" if self.training else "validation"
+            preds = torch.cat([tmp['preds'] for tmp in outputs])
+            targets = torch.cat([tmp['target'] for tmp in outputs])
+            conf_mat = confusion_matrix(preds, targets, num_classes=2)
+            conf_mar = torch.flip(conf_mat, [0, 1])
+
+            df_cm = pd.DataFrame(confusion_matrix.numpy(), index = range(2), columns=range(2))
+            plt.figure(figsize = (10,7))
+            fig_ = sns.heatmap(df_cm, annot=True, cmap='Spectral').get_figure()
+            plt.close(fig_)
+            
+            self.logger.experiment.add_figure("contact_%s_confmat" % mode, fig_, self.current_epoch)
 
     def training_step(self, batch_data: Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]], batch_idx: int) -> torch.Tensor:
         """
@@ -196,7 +223,27 @@ class MSAModel(pl.LightningModule):
             batch_idx (int): Batch number.
         """
 
-        self._step(batch_data, batch_idx)
+        return self._step(batch_data, batch_idx)
+    
+    def training_epoch_end(self, outputs: List[Any]) -> None:
+        """
+        Is invoked at the end of a training epoch.
+
+        Args:
+            outputs (List[Any]): Outputs from training steps.
+        """
+        
+        self._epoch_end(outputs)
+    
+    def validation_epoch_end(self, outputs: List[Any]) -> None:
+        """
+        Is invoked at the end of a validation epoch.
+
+        Args:
+            outputs (List[Any]): Outputs from validation steps.
+        """
+        
+        self._epoch_end(outputs)
 
     def configure_optimizers(self) -> Dict[str, Any]:
         """
