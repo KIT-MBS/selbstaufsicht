c5ca289ad54b3ef81860dec4a622d2167c77655f
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2022-02-25 17:43:35 +0100

    Fixed wrong parameter count for ContactHead

--- selbstaufsicht/models/self_supervised/msa/modules.py
+++ selbstaufsicht/models/self_supervised/msa/modules.py
@@ -146,7 +146,7 @@ class ContactHead(nn.Module):
         super(ContactHead, self).__init__()
         self.num_maps = num_maps
         self.cull_tokens = cull_tokens
-        self.l = nn.Conv2d(num_maps, num_maps, kernel_size=1, bias=False, **factory_kwargs)
+        self.l = nn.Conv2d(num_maps, 1, kernel_size=1, bias=False, **factory_kwargs)
         self.f = nn.Sigmoid()
 
     def forward(self, latent, x) -> torch.Tensor:
@@ -169,9 +169,8 @@ class ContactHead(nn.Module):
         out = x['attn_maps']
         out = torch.cat([m[0].squeeze(dim=2) for m in out], dim=1) # [B, num_blocks * H, L, L]
         out = out.masked_select(mask).reshape(B, self.num_maps, degapped_L, degapped_L)
-        out = self.l(out)
+        out = self.l(out)  # [B, 1, L, L]
         # TODO this is some hackery to use the ignore_index of NLLLoss, since BCELoss does not have it
-        out = out.sum(dim=1, keepdim=True)  # [B, 1, L, L]
         out = self.f(out)
         out = torch.cat((1. - out, out), dim=1)  # [B, 2, L, L]
         out = torch.log(out)
