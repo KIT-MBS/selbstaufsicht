0909f01ad61e45f54b178d8cd260ddd8415a8d54
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2022-03-11 10:36:06 +0100

    Added k-fold cross-validation to downstream

--- scripts/train_downstream.py
+++ scripts/train_downstream.py
@@ -10,6 +10,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.plugins import DDPPlugin
 
@@ -40,6 +41,8 @@ def main():
     parser.add_argument('--disable-progress-bar', action='store_true', help="disables the training progress bar")
     parser.add_argument('--disable-shuffle', action='store_true', help="disables the dataset shuffling")
     parser.add_argument('--rng-seed', default=42, type=int, help="Random number generator seed")
+    parser.add_argument('--validation-ratio', default=0.1, type=float, help="Ratio of the validation dataset w.r.t. the full training dataset, if k-fold cross validation is disabled.")
+    parser.add_argument('--cv-num-folds', default=1, type=int, help="Number of folds in k-fold cross validation. If 1, then cross validation is disabled.")
     # Data parallelism
     parser.add_argument('--num-gpus', default=-1, type=int, help="Number of GPUs per node. -1 refers to using all available GPUs. 0 refers to using the CPU.")
     parser.add_argument('--num-nodes', default=1, type=int, help="Number of nodes")
@@ -54,8 +57,6 @@ def main():
     torch.manual_seed(args.rng_seed)
     np.random.seed(args.rng_seed)
     random.seed(args.rng_seed)
-    data_loader_rng = torch.Generator()
-    data_loader_rng.manual_seed(args.rng_seed)
 
     num_gpus = args.num_gpus if args.num_gpus >= 0 else torch.cuda.device_count()
     if num_gpus * args.num_nodes > 1:
@@ -68,17 +69,16 @@ def main():
     h_params = checkpoint['hyper_parameters']
 
     learning_rate = 0.0001
-
-    root = os.environ['DATA_PATH']
+    
     downstream_transform = get_downstream_transforms(subsample_depth=h_params['subsampling_depth'], subsample_mode=args.subsampling_mode, threshold=args.distance_threshold)
-    train_metrics, val_metrics = get_downstream_metrics()
-    downstream_ds = datasets.CoCoNetDataset(root, 'train', transform=downstream_transform)
-    test_ds = datasets.CoCoNetDataset(root, 'test', transform=downstream_transform)
+    kfold_cv_downstream = datasets.KFoldCVDownstream(downstream_transform, num_folds=args.cv_num_folds, val_ratio=args.validation_ratio, batch_size=args.batch_size, shuffle=not args.disable_shuffle, rng_seed=args.rng_seed)
 
     dt_now = datetime.now()
     log_dir = h_params['log_dir'] if args.log_dir == "" else args.log_dir
     log_exp_name = h_params['log_exp_name'] if args.log_exp_name == "" else args.log_exp_name
     log_run_name = "downstream__" + h_params['log_run_name'] if args.log_run_name == "" else dt_now.strftime(args.log_run_name)
+    if args.cv_num_folds >= 2:
+        log_exp_name = log_run_name
 
     jigsaw_euclid_emb = None
     if 'jigsaw_euclid_emb' in h_params and h_params['jigsaw_euclid_emb']:
@@ -99,86 +99,83 @@ def main():
         tasks.append("jigsaw")
     if h_params['task_contrastive']:
         tasks.append("contrastive")
-    _, task_heads, task_losses, _, _ = get_tasks(tasks,
-                                       h_params['feature_dim_head'] * h_params['num_heads'],
-                                       subsample_depth=h_params['subsampling_depth'],
-                                       subsample_mode=h_params['subsampling_mode'],
-                                       crop_size=h_params['cropping_size'],
-                                       crop_mode=h_params['cropping_mode'],
-                                       masking=h_params['inpainting_masking_type'],
-                                       p_mask=h_params['inpainting_masking_p'],
-                                       jigsaw_partitions=h_params['jigsaw_partitions'],
-                                       jigsaw_classes=h_params['jigsaw_permutations'],
-                                       jigsaw_linear=not h_params['jigsaw_nonlinear'],
-                                       jigsaw_delimiter= jigsaw_delimiter,
-                                       jigsaw_euclid_emb=jigsaw_euclid_emb,
-                                       simclr_temperature=h_params['contrastive_temperature'])
+        
+    for fold_idx in range(args.cv_num_folds):
+        train_metrics, val_metrics = get_downstream_metrics()
+        _, task_heads, task_losses, _, _ = get_tasks(tasks,
+                                        h_params['feature_dim_head'] * h_params['num_heads'],
+                                        subsample_depth=h_params['subsampling_depth'],
+                                        subsample_mode=h_params['subsampling_mode'],
+                                        crop_size=h_params['cropping_size'],
+                                        crop_mode=h_params['cropping_mode'],
+                                        masking=h_params['inpainting_masking_type'],
+                                        p_mask=h_params['inpainting_masking_p'],
+                                        jigsaw_partitions=h_params['jigsaw_partitions'],
+                                        jigsaw_classes=h_params['jigsaw_permutations'],
+                                        jigsaw_linear=not h_params['jigsaw_nonlinear'],
+                                        jigsaw_delimiter= jigsaw_delimiter,
+                                        jigsaw_euclid_emb=jigsaw_euclid_emb,
+                                        simclr_temperature=h_params['contrastive_temperature'])
 
-    if args.re_init:
-        model = models.self_supervised.MSAModel(
-                num_blocks = h_params['num_blocks'],
-                num_heads = h_params['num_heads'],
-                dim_head = h_params['feature_dim_head'],
-                task_heads=task_heads,
-                task_losses=task_losses,
-                alphabet_size=len(downstream_ds.token_mapping),
-                padding_token=downstream_ds.token_mapping['PADDING_TOKEN'],
-                lr=args.learning_rate,
-                lr_warmup=args.learning_rate_warmup,
-                dropout=args.dropout,
-                emb_grad_freq_scale=not h_params['disable_emb_grad_freq_scale'],
-                fix_backbone=args.fix_backbone
-                )
-    else:
-        model = models.self_supervised.MSAModel.load_from_checkpoint(
-                checkpoint_path = args.checkpoint,
-                num_blocks = h_params['num_blocks'],
-                num_heads = h_params['num_heads'],
-                feature_dim_head = h_params['feature_dim_head'],
-                task_heads=task_heads,
-                task_losses=task_losses,
-                alphabet_size=len(downstream_ds.token_mapping),
-                padding_token=downstream_ds.token_mapping['PADDING_TOKEN'],
-                lr=args.learning_rate,
-                lr_warmup=args.learning_rate_warmup,
-                dropout=args.dropout,
-                emb_grad_freq_scale=not h_params['disable_emb_grad_freq_scale'],
-                fix_backbone=args.fix_backbone
-                )
-    model.tasks = ['contact']
-    model.losses['contact'] = nn.NLLLoss(weight=torch.tensor([1-args.loss_contact_weight, args.loss_contact_weight]), ignore_index=-1)
-    model.task_heads['contact'] = models.self_supervised.msa.modules.ContactHead(h_params['num_blocks'] * h_params['num_heads'], cull_tokens=[downstream_ds.token_mapping[l] for l in ['-', '.', 'START_TOKEN', 'DELIMITER_TOKEN']])
-    model.need_attn = True
-    model.task_loss_weights = {'contact': 1.}
-    model.train_metrics = train_metrics
-    model.val_metrics = val_metrics
+        if args.re_init:
+            model = models.self_supervised.MSAModel(
+                    num_blocks = h_params['num_blocks'],
+                    num_heads = h_params['num_heads'],
+                    dim_head = h_params['feature_dim_head'],
+                    task_heads=task_heads,
+                    task_losses=task_losses,
+                    alphabet_size=len(downstream_ds.token_mapping),
+                    padding_token=downstream_ds.token_mapping['PADDING_TOKEN'],
+                    lr=args.learning_rate,
+                    lr_warmup=args.learning_rate_warmup,
+                    dropout=args.dropout,
+                    emb_grad_freq_scale=not h_params['disable_emb_grad_freq_scale'],
+                    fix_backbone=args.fix_backbone)
+        else:
+            model = models.self_supervised.MSAModel.load_from_checkpoint(
+                    checkpoint_path = args.checkpoint,
+                    num_blocks = h_params['num_blocks'],
+                    num_heads = h_params['num_heads'],
+                    feature_dim_head = h_params['feature_dim_head'],
+                    task_heads=task_heads,
+                    task_losses=task_losses,
+                    alphabet_size=len(downstream_ds.token_mapping),
+                    padding_token=downstream_ds.token_mapping['PADDING_TOKEN'],
+                    lr=args.learning_rate,
+                    lr_warmup=args.learning_rate_warmup,
+                    dropout=args.dropout,
+                    emb_grad_freq_scale=not h_params['disable_emb_grad_freq_scale'],
+                    fix_backbone=args.fix_backbone)
+        model.tasks = ['contact']
+        model.losses['contact'] = nn.NLLLoss(weight=torch.tensor([1-args.loss_contact_weight, args.loss_contact_weight]), ignore_index=-1)
+        model.task_heads['contact'] = models.self_supervised.msa.modules.ContactHead(h_params['num_blocks'] * h_params['num_heads'], cull_tokens=[downstream_ds.token_mapping[l] for l in ['-', '.', 'START_TOKEN', 'DELIMITER_TOKEN']])
+        model.need_attn = True
+        model.task_loss_weights = {'contact': 1.}
+        model.train_metrics = train_metrics
+        model.val_metrics = val_metrics
+        
+        kfold_cv_downstream.setup_fold_index(fold_idx)
 
-    train_dl = DataLoader(downstream_ds,
-                          batch_size=args.batch_size,
-                          shuffle=not args.disable_shuffle,
-                          num_workers=0,
-                          worker_init_fn=partial(data_loader_worker_init, rng_seed=args.rng_seed),
-                          generator=data_loader_rng,
-                          pin_memory=False)
-    test_dl = DataLoader(test_ds,
-                         batch_size=args.batch_size,
-                         shuffle=False,
-                         num_workers=0,
-                         worker_init_fn=partial(data_loader_worker_init, rng_seed=args.rng_seed),
-                         generator=data_loader_rng,
-                         pin_memory=False)
+        train_dl = kfold_cv_downstream.train_dataloader()
+        val_dl = kfold_cv_downstream.val_dataloader()
+        
+        if args.cv_num_folds >= 2:
+            log_run_name = 'fold_%d' % (fold_idx + 1)
 
-    tb_logger = TensorBoardLogger(save_dir=log_dir, name=log_exp_name, version=log_run_name)
-    trainer = Trainer(max_epochs=args.num_epochs,
-                      gpus=args.num_gpus,
-                      auto_select_gpus=num_gpus > 0,
-                      num_nodes=args.num_nodes,
-                      precision=args.precision,
-                      strategy=dp_strategy,
-                      enable_progress_bar=not args.disable_progress_bar,
-                      log_every_n_steps=args.log_every,
-                      logger=tb_logger)
-    trainer.fit(model, train_dl, test_dl)
+        tb_logger = TensorBoardLogger(save_dir=log_dir, name=log_exp_name, version=log_run_name)
+        checkpoint_callback = ModelCheckpoint(monitor='contact_validation_topLprec', filename="downstream-{epoch:02d}-{contact_validation_topLprec:.4f}", mode='max')
+        
+        trainer = Trainer(max_epochs=args.num_epochs,
+                          gpus=args.num_gpus,
+                          auto_select_gpus=num_gpus > 0,
+                          num_nodes=args.num_nodes,
+                          precision=args.precision,
+                          strategy=dp_strategy,
+                          enable_progress_bar=not args.disable_progress_bar,
+                          log_every_n_steps=args.log_every,
+                          logger=tb_logger,
+                          callbacks=[checkpoint_callback])
+        trainer.fit(model, train_dl, val_dl)
 
 if __name__ == '__main__':
     main()
---
+++ selbstaufsicht/datasets/kfold_cv_downstream.py
@@ -0,0 +1,68 @@
+from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader, Subset, random_split
+
+from selbstaufsicht.datasets import CoCoNetDataset
+from selbstaufsicht.utils import data_loader_worker_init
+
+
+class KFoldCVDownstream():
+    def __init__(self, transform, num_folds: int = 1, val_ratio: float = 0.1, batch_size: int = 1, shuffle: bool = True, rng_seed: int = 42) -> None:
+        """
+        Initializes K-fold cross validation for the downstream task.
+
+        Args:
+            transform: Data transforms
+            num_folds (int, optional): Number of folds. Defaults to 1.
+            val_ratio (float, optional): Validation dataset ratio in case of disabled cross validation (num_folds=1). Defaults to 0.1.
+            batch_size (int, optional): Batch size. Defaults to 1.
+            shuffle (bool, optional): Whether data is shuffled. Defaults to True.
+            rng_seed (int, optional): DataLoader/Shuffling RNG seed. Defaults to 42.
+        """
+        
+        assert num_folds > 0
+        assert 0 < val_ratio < 1
+        
+        self.num_folds = num_folds
+        self.val_ratio = val_ratio
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.rng_seed = rng_seed
+        
+        # set rng seed
+        self.data_loader_rng = torch.Generator()
+        self.data_loader_rng.manual_seed(rng_seed)
+        
+        # load dataset
+        root = os.environ['DATA_PATH']
+        self.train_dataset = CoCoNetDataset(root, 'train', transform=transform)
+        
+        # setup splits
+        if self.num_folds == 1:
+            val_size = int(self.val_ratio * len(self.train_dataset))
+            train_size = len(self.train_dataset) - val_size
+            self.train_fold, self.val_fold = random_split(self.train_dataset, [train_size, val_size], self.data_loader_rng)
+        else:
+            self.splits = [split for split in KFold(self.num_folds, shuffle=self.shuffle, random_state=self.rng_seed).split(range(len(self.train_dataset)))]
+    
+    def setup_fold_index(self, fold_index: int) -> None:
+        """
+        Setups concrete cross validation fold.
+
+        Args:
+            fold_index (int): Fold index.
+        """
+        
+        if self.num_folds >= 2:
+            train_indices, val_indices = self.splits[fold_index]
+            self.train_fold = Subset(self.train_dataset, train_indices)
+            self.val_fold = Subset(self.train_dataset, val_indices)
+    
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.train_fold, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=0, 
+                          worker_init_fn=partial(data_loader_worker_init, rng_seed=self.rng_seed), 
+                          generator=self.data_loader_rng, pin_memory=False)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.val_fold, batch_size=self.batch_size, shuffle=False, num_workers=0, 
+                          worker_init_fn=partial(data_loader_worker_init, rng_seed=self.rng_seed), 
+                          generator=self.data_loader_rng, pin_memory=False)
