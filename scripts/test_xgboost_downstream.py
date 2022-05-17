86ac8bfbbb0a0411b3439c4c89115979a3db07f7
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2022-05-17 16:50:16 +0200

    Several fixes

--- scripts/test_xgboost_downstream.py
+++ scripts/test_xgboost_downstream.py
@@ -4,7 +4,7 @@ from functools import partial
 import json
 import random
 import os
-from typing import Dict
+from typing import Dict, Tuple
 
 import numpy as np
 import pandas as pd
@@ -34,7 +34,7 @@ def sigmoid(x: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-x))
 
 
-def xgb_topkLPrec_var_k(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: np.ndarray, L_mapping: np.ndarray, k: np.ndarray, treat_all_preds_positive: bool = False) -> Tuple[Dict[float, np.ndarray], Dict[float, np.ndarray]]:
+def xgb_topkLPrec_var_k(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: np.ndarray, L_mapping: np.ndarray, k: np.ndarray, treat_all_preds_positive: bool = True) -> Tuple[Dict[float, np.ndarray], Dict[int, np.ndarray]]:
     """
     Custom XGBoost Metric for top-L-precision with support for various k.
 
@@ -44,7 +44,7 @@ def xgb_topkLPrec_var_k(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: np.n
         msa_mapping (np.ndarray): Mapping: Data point -> MSA index [B].
         L_mapping (np.ndarray): Mapping: MSA index -> MSA L.
         k (np.ndarray): Coefficients k that are used in computing the top-(k*L)-precision [num_k, num_msa].
-        treat_all_preds_positive (bool, optional): Whether all non-ignored preds are treated as positives, analogous to the CocoNet paper. Defaults to False.
+        treat_all_preds_positive (bool, optional): Whether all non-ignored preds are treated as positives, analogous to the CocoNet paper. Defaults to True.
 
     Returns:
         Tuple[Dict[float, np.ndarray], Dict[float, np.ndarray]]: Metric values per k (relative); Metric values per k*L (absolute).
@@ -66,7 +66,7 @@ def xgb_topkLPrec_var_k(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: np.n
         y_ = y[mask]
         
         for k_idx in range(num_k):
-            k_ = k[k_idx, msa_idx]
+            k_ = k[k_idx]
             L = L_mapping[msa_idx]
             k_L = min(max(1, int(k_*L)), len(y_))
             L_idx = np.argpartition(preds_, -k_L)[-k_L:]  # [k*L]
@@ -90,6 +90,9 @@ def xgb_topkLPrec_var_k(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: np.n
             
             top_l_prec_dict_rel[k_].append(top_l_prec)
             top_l_prec_dict_abs[k_L].append(top_l_prec)
+            
+            if k_L == len(y_):
+                break
     
     top_l_prec_dict_rel = {k: np.array(v) for k, v in top_l_prec_dict_rel.items()}
     top_l_prec_dict_abs = {k: np.array(v) for k, v in top_l_prec_dict_abs.items()}
@@ -197,13 +200,13 @@ def plot_contact_maps(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: np.nda
         fig.savefig(os.path.join(save_dir, '%d.pdf' % msa_idx))
 
 
-def plot_top_l_prec_over_k(top_l_prec_dict_rel: Dict[float, np.ndarray], top_l_prec_dict_abs: Dict[float, np.ndarray], save_dir: str) -> None:
+def plot_top_l_prec_over_k(top_l_prec_dict_rel: Dict[float, np.ndarray], top_l_prec_dict_abs: Dict[int, np.ndarray], save_dir: str) -> None:
     """
     Creates plots for top-(k*L)-precision over k and (k*L), respectively.
 
     Args:
         top_l_prec_dict_rel (Dict[float, np.ndarray]): Metric values per k (relative). 
-        top_l_prec_dict_abs (Dict[float, np.ndarray]): Metric values per k*L (absolute).
+        top_l_prec_dict_abs (Dict[int, np.ndarray]): Metric values per k*L (absolute).
         save_dir (str): Directory, where plots are saved.
     """
     
@@ -237,8 +240,8 @@ def main():
     # Test process
     parser.add_argument('--batch-size', default=1, type=int, help="Batch size (local in case of multi-gpu testing)")
     parser.add_argument('--min-k', default=0.01, type=float, help="Minimum coefficient k that is used in computing the top-(k*L)-precision.")
-    parser.add_argument('--max-k', default=-1, type=float, help="Maximum coefficient k that is used in computing the top-(k*L)-precision. -1 refers to maximum L/2.")
-    parser.add_argument('--num-k', default=100, type=int, help="Number of samples for k used in computing the top-(k*L)-precision. 1 disables top-(k*L)-precision over k plot and uses min-k as k.")
+    parser.add_argument('--max-k', default=-1, type=float, help="Maximum coefficient k that is used in computing the top-(k*L)-precision. -1 refers to maximum L/2 of the longest sequence.")
+    parser.add_argument('--num-k', default=500, type=int, help="Number of samples for k used in computing the top-(k*L)-precision. 1 disables top-(k*L)-precision over k plot and uses min-k as k.")
     parser.add_argument('--treat-all-preds-positive', action='store_true', help="Whether all non-ignored preds are treated as positives, analogous to the CocoNet paper.")
     # Visualization
     parser.add_argument('--vis-dir', type=str, default='', help="Directory, where plots are saved. If empty, no plots are created.")
@@ -406,12 +409,12 @@ def main():
         top_l_prec = xgb_topkLPrec(preds, test_data, msa_mapping_filtered, L_mapping, args.min_k, args.treat_all_preds_positive)
         print("Top-%sL-Prec:" % str(args.top_l_prec_coeff), top_l_prec)
     else:
-        min_k = np.full((len(preds)), args.min_k)
+        min_k = args.min_k
         if args.max_k == -1:
-            max_k = L_mapping / 2
+            max_k = max(L_mapping / 2)
         else:
-            max_k = np.full((len(preds)), args.max_k)
-        k_range = np.linspace(min_k, max_k, args.num_k)  # [num_k, num_msa]
+            max_k = args.max_k
+        k_range = np.linspace(min_k, max_k, args.num_k)  # [num_k]
         
         top_l_prec_dict_rel, top_l_prec_dict_abs = xgb_topkLPrec_var_k(preds, test_data, msa_mapping_filtered, L_mapping, k_range, args.treat_all_preds_positive)
         print("Relative Top-kL_Prec:", top_l_prec_dict_rel)
