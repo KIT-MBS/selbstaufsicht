fixed max seqlen computation

selbstaufsicht/models/self_supervised/msa/modules.py
@@ -98,10 +98,10 @@ class JigsawHead(nn.Module):
 
         # latent is of shape [B, E, L, D]
         if self.frozen:
-            latent = latent[:,0,0,:] #[B,D]
+            latent = latent[:, 0, 0, :]  # [B,D]
         else:
             latent = latent[:, :, 0, :]  # [B, E, D]
-        
+
         if self.euclid_emb:
             return self.proj(latent)  # [B, E, NClasses]
         else:

selbstaufsicht/models/self_supervised/msa/transforms.py
@@ -85,6 +85,7 @@ class MSACropping():
             if contrastive_msa.get_alignment_length() > self.length:
                 contrastive_msa = self.cropping_fn(contrastive_msa, self.length, True)
             x['contrastive'] = contrastive_msa
+
         return x, y
 
 
@@ -229,8 +230,8 @@ class RandomMSAShuffling():
         if self.euclid_emb is None:
 
             if self.frozen:
-                y['jigsaw']=perm_sampling[0]
-                y['jigsaw']=y['jigsaw'].reshape(1)
+                y['jigsaw'] = perm_sampling[0]
+                y['jigsaw'] = y['jigsaw'].reshape(1)
             else:
                 y['jigsaw'] = perm_sampling
         else:
@@ -381,7 +382,7 @@ class ExplicitPositionalEncoding():
         seqlen = msa.size(-1)
         if seqlen > self.max_seqlen:
             raise ValueError(f'Sequence dimension in input too large: {seqlen} > {self.max_seqlen}')
-        absolute = torch.arange(1, seqlen + 1, dtype=torch.long).unsqueeze(0)
+        absolute = torch.arange(0, seqlen, dtype=torch.long).unsqueeze(0)
         if 'aux_features' not in x:
             x['aux_features'] = absolute
         else:

tests/test_preprocessing.py
@@ -74,14 +74,14 @@ def test_msa_mask_token_static(tokenized_sample):
     x, y = masking(*tokenized_sample)
     x, y = positional(x, y)
 
-    x_ref = {'aux_features': torch.tensor([[1,
+    x_ref = {'aux_features': torch.tensor([[0,
+                                            1,
                                             2,
                                             3,
                                             4,
                                             5,
                                             6,
-                                            7,
-                                            8]]),
+                                            7, ]]),
              'msa': torch.tensor([[17,  3, 19,  4, 19,  5, 19,  3],
                                   [17, 19,  3,  4,  1,  5,  4, 19],
                                   [17,  5, 19,  4, 19, 19, 19,  1],
@@ -109,14 +109,14 @@ def test_msa_mask_token_nonstatic(tokenized_sample):
     x, y = masking(*tokenized_sample)
     x, y = positional(x, y)
 
-    x_ref = {'aux_features': torch.tensor([[1,
+    x_ref = {'aux_features': torch.tensor([[0,
+                                            1,
                                             2,
                                             3,
                                             4,
                                             5,
                                             6,
-                                            7,
-                                            8]]),
+                                            7, ]]),
              'msa': torch.tensor([[17, 3, 5, 4, 0, 5, 5, 3],
                                   [17, 0, 3, 4, 1, 5, 4, 3],
                                   [17, 5, 4, 4, 2, 3, 2, 1],
@@ -144,7 +144,7 @@ def test_msa_mask_column_static(tokenized_sample):
     x, y = masking(*tokenized_sample)
     x, y = positional(x, y)
 
-    x_ref = {'aux_features': torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]]),
+    x_ref = {'aux_features': torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]]),
              'msa': torch.tensor([[17, 3, 5, 19, 5, 19, 4, 19],
                                   [17, 3, 3, 19, 1, 19, 4, 19],
                                   [17, 5, 5, 19, 3, 19, 4, 19],
@@ -171,7 +171,7 @@ def test_msa_mask_column_nonstatic(tokenized_sample):
     x, y = masking(*tokenized_sample)
     x, y = positional(x, y)
 
-    x_ref = {'aux_features': torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]]),
+    x_ref = {'aux_features': torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]]),
              'msa': torch.tensor([[17, 3, 5, 0, 5, 0, 4, 4],
                                   [17, 3, 3, 4, 1, 0, 4, 4],
                                   [17, 5, 5, 2, 3, 4, 4, 5],
@@ -198,7 +198,7 @@ def test_msa_mask_block_static(tokenized_sample):
     x, y = masking(*tokenized_sample)
     x, y = positional(x, y)
 
-    x_ref = {'aux_features': torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]]),
+    x_ref = {'aux_features': torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]]),
              'msa': torch.tensor([[17, 3, 5, 19, 19, 19, 4, 3],
                                   [17, 3, 3, 19, 19, 19, 4, 3],
                                   [17, 5, 5, 19, 19, 19, 4, 1],
@@ -226,7 +226,7 @@ def test_msa_mask_block_nonstatic(tokenized_sample):
     x, y = masking(*tokenized_sample)
     x, y = positional(x, y)
 
-    x_ref = {'aux_features': torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]]),
+    x_ref = {'aux_features': torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]]),
              'msa': torch.tensor([[17, 3, 5, 5, 4, 3, 4, 3],
                                   [17, 3, 3, 5, 5, 2, 4, 3],
                                   [17, 5, 5, 0, 0, 4, 4, 1],