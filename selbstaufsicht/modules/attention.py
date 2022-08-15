43dabf00ca678acfc27812193c15ce95f5c012e7
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2022-08-15 18:36:58 +0200

    Fixed attn mask shape

--- selbstaufsicht/modules/attention.py
+++ selbstaufsicht/modules/attention.py
@@ -464,7 +464,7 @@ class FastSelfAttention2d(nn.Module):
             attn_maps = attn_maps.softmax(dim=-1)  # [B, H, L, L]
 
         if attn_mask is not None:
-            attn_mask = attn_mask.view(B * self.num_heads, E, L, 1).view(B * self.num_heads, E * L, 1).expand(-1, -1, self.dim_head) # [B * H, E * L, DH]
+            attn_mask = attn_mask.view(B * self.num_heads, E, L, 1).view(B * self.num_heads, E * L, 1).expand(-1, -1, self.num_features) # [B * H, E * L, M]
         
         q = q.view(B, E * L, self.num_heads, self.dim_head).permute(0, 2, 1, 3).view(B * self.num_heads, E * L, self.dim_head)  # [B * H, E * L, DH]
         k = k.view(B, E * L, self.num_heads, self.dim_head).permute(0, 2, 1, 3).view(B * self.num_heads, E * L, self.dim_head)  # [B * H, E * L, DH]
