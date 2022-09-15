6bf3d4fafd13c57ddfec29162ca8a545c3fedaa3
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2022-09-15 15:21:34 +0200

    Fixed data overwriting

--- scripts/test_xgboost_downstream.py
+++ scripts/test_xgboost_downstream.py
@@ -49,19 +49,19 @@ def store_contact_maps_data(preds: np.ndarray, dtest: xgb.DMatrix, msa_mapping: 
         mask = msa_mapping == msa_idx  # [B]
         L = L_mapping[msa_idx]
 
-        preds_ = preds_[mask].reshape((L, L))
-        y_ = y_[mask].reshape((L, L)).astype(bool)
+        preds_shaped = preds_[mask].reshape((L, L))
+        y_shaped = y_[mask].reshape((L, L)).astype(bool)
         
         if top_l:
-            kL = min(max(1, int(L)), len(y_))
-            L_idx = np.c_[np.unravel_index(np.argpartition(preds_.ravel(), -kL)[-kL:], preds_.shape)]
-            preds_ = np.zeros_like(y_)
-            preds_[L_idx[:, 0], L_idx[:, 1]] = True
+            kL = min(max(1, int(L)), len(y_shaped))
+            L_idx = np.c_[np.unravel_index(np.argpartition(preds_shaped.ravel(), -kL)[-kL:], preds_shaped.shape)]
+            preds_shaped = np.zeros_like(y_shaped)
+            preds_shaped[L_idx[:, 0], L_idx[:, 1]] = True
         else:
-            preds_ = xgb_contact.sigmoid(preds_).astype(bool)
+            preds_shaped = xgb_contact.sigmoid(preds_shaped).astype(bool)
         
-        np.save(os.path.join(save_dir, '%s_preds.npy' % pdb_ids[msa_idx]), preds_)
-        np.save(os.path.join(save_dir, '%s_target.npy' % pdb_ids[msa_idx]), y_)
+        np.save(os.path.join(save_dir, '%s_preds.npy' % pdb_ids[msa_idx]), preds_shaped)
+        np.save(os.path.join(save_dir, '%s_target.npy' % pdb_ids[msa_idx]), y_shaped)
 
 
 def store_top_l_prec_over_k_data(top_l_prec_dict_rel: Dict[float, np.ndarray], top_l_prec_list_abs: Dict[int, Dict[int, float]], pdb_ids: List[str], save_dir: str) -> None:
@@ -187,7 +187,7 @@ def main():
     if args.vis_dir != '' and args.vis_contact_maps:
         contact_map_plot_dir = os.path.join(args.vis_dir, 'contact_maps')
         os.makedirs(contact_map_plot_dir, exist_ok=True)
-        store_contact_maps_data(preds, test_data, msa_mapping_filtered, msa_mask, L_mapping, pdb_ids, args.vis_top_l, contact_map_plot_dir)
+        store_contact_maps_data(preds, test_data, msa_mapping, msa_mask, L_mapping, pdb_ids, args.vis_top_l, contact_map_plot_dir)
 
 
 if __name__ == '__main__':
