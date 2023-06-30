b7854c6202024df3f9ca775f760f6aeb6ad6137b
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2023-06-30 15:55:37 +0200

    Added thermostable task to cv eval scripts

--- scripts/eval_downstream_cv.py
+++ scripts/eval_downstream_cv.py
@@ -8,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='Selbstaufsicht Weakly Supervised Contact Prediction Script')
     # Pre-trained model
     parser.add_argument('--log-path', type=str, help="Path to logging data of the specific downstream run to be evaluated.")
+    parser.add_argument('--task', default='contact', type=str, help="Downstream task ('contact', 'thermostable')")
 
     args = parser.parse_args()
 
@@ -15,31 +16,37 @@ def main():
     num_folds = len(fold_dirs)
 
     num_epochs_arr = []
-    top_l_prec_arr = []
+    metric_arr = []
 
     for fold_dir in fold_dirs:
         checkpoint_path = os.path.join(args.log_path, fold_dir, 'checkpoints')
         checkpoint = [item for item in os.listdir(checkpoint_path) if 'downstream' in item]
         assert len(checkpoint) == 1
-        checkpoint = checkpoint[0].replace('downstream-epoch=', '').replace('-contact_validation_topLprec', '').replace('.ckpt', '')
+        if args.task == 'contact':
+            checkpoint = checkpoint[0].replace('downstream-epoch=', '').replace('-contact_validation_topLprec', '').replace('.ckpt', '')
+        elif args.task == 'thermostable':
+            checkpoint = checkpoint[0].replace('downstream-epoch=', '').replace('-thermostable_validation_pcorr', '').replace('.ckpt', '')
         splits = checkpoint.split('=')
         assert len(splits) == 2
-        num_epochs, top_l_prec = splits[0], splits[1]
+        num_epochs, metric = splits[0], splits[1]
         num_epochs = float(num_epochs)
-        top_l_prec = float(top_l_prec)
+        metric = float(metric)
         num_epochs_arr.append(num_epochs)
-        top_l_prec_arr.append(top_l_prec)
+        metric_arr.append(metric)
 
     num_epochs_arr = np.array(num_epochs_arr)
-    top_l_prec_arr = np.array(top_l_prec_arr)
+    metric_arr = np.array(metric_arr)
 
     num_epochs_mean = num_epochs_arr.mean()
     num_epochs_std = num_epochs_arr.std(ddof=1)
 
-    top_l_prec_mean = top_l_prec_arr.mean()
-    top_l_prec_std = top_l_prec_arr.std(ddof=1)
+    metric_mean = metric_arr.mean()
+    metric_std = metric_arr.std(ddof=1)
 
-    print(f"{num_folds}-fold cross-validation: {top_l_prec_mean} +- {top_l_prec_std} top-L-precision after {num_epochs_mean} +- {num_epochs_std} epochs.")
+    if args.task == 'contact':
+        print(f"{num_folds}-fold cross-validation: {metric_mean} +- {metric_std} top-L-precision after {num_epochs_mean} +- {num_epochs_std} epochs.")
+    elif args.task == 'thermostable':
+        print(f"{num_folds}-fold cross-validation: {metric_mean} +- {metric_std} Pearson-corrcoef after {num_epochs_mean} +- {num_epochs_std} epochs.")
 
 
 if __name__ == '__main__':
